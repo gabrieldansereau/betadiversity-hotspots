bbaf14ba0c8cebc68088ea1702341585d203353e
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-08-17 14:15:33 -0400

    use a cleaner interface for SDM layers

--- lib/SDMLayer.jl
+++ lib/SDMLayer.jl
@@ -1,4 +1,4 @@
-struct SDMPredictor{T<:Number}
+struct SDMLayer{T<:Number}
     grid::Matrix{T}
     left::Float64
     right::Float64
@@ -6,27 +6,27 @@ struct SDMPredictor{T<:Number}
     top::Float64
 end
 
-function latitudes(p::SDMPredictor)
+function latitudes(p::SDMLayer)
     n = size(p.grid, 1)
     grid_size = (p.top-p.bottom)/n/2.0
     centers = range(p.bottom+grid_size; stop=p.top-grid_size, length=n)
     return centers
 end
 
-function longitudes(p::SDMPredictor)
+function longitudes(p::SDMLayer)
     n = size(p.grid, 2)
     grid_size = (p.right-p.left)/n/2.0
     centers = range(p.left+grid_size; stop=p.right-grid_size, length=n)
     return centers
 end
 
-function Base.getindex(p::SDMPredictor, longitude::Float64, latitude::Float64)
+function Base.getindex(p::SDMLayer, longitude::Float64, latitude::Float64)
     i_lon = findmin(abs.(longitude .- longitudes(p)))[2]
     j_lat = findmin(abs.(latitude .- latitudes(p)))[2]
     return p.grid[j_lat, i_lon]
 end
 
-function Base.getindex(p::SDMPredictor, longitude::NTuple{2,Float64}, latitude::NTuple{2,Float64})
+function Base.getindex(p::SDMLayer, longitude::NTuple{2,Float64}, latitude::NTuple{2,Float64})
     m_lon = findmin(abs.(minimum(longitude) .- longitudes(p)))[2]
     M_lon = findmin(abs.(maximum(longitude) .- longitudes(p)))[2]
     m_lat = findmin(abs.(minimum(latitude) .- latitudes(p)))[2]
@@ -41,13 +41,13 @@ function Base.getindex(p::SDMPredictor, longitude::NTuple{2,Float64}, latitude::
     n_top = latitudes(p)[M_lat]+n_lat
 
     n_grid = p.grid[m_lat:M_lat, m_lon:M_lon]
-    return SDMPredictor(n_grid, n_left, n_right, n_bottom, n_top)
+    return SDMLayer(n_grid, n_left, n_right, n_bottom, n_top)
 end
 
-function Base.size(p::SDMPredictor)
+function Base.size(p::SDMLayer)
     return size(p.grid)
 end
 
-function Base.size(p::SDMPredictor, i::Int64)
+function Base.size(p::SDMLayer, i::Int64)
     return size(p.grid, i)
 end
--- main.jl
+++ main.jl
@@ -4,7 +4,7 @@ using GDAL
 using GBIF
 
 # Core type
-include("lib/SDMPredictor.jl")
+include("lib/SDMLayer.jl")
 
 # Read geotiff files
 include("lib/gdal.jl")
@@ -12,64 +12,46 @@ include("lib/gdal.jl")
 # Format bioclim data
 include("lib/worldclim.jl")
 
-# Other
-include("distributions.jl")
-
-
-predicates = get_bioclim_values(occ_data, bioclim_variables, longitudes, latitudes)
-
-b_box = [(-155.0, 20.0),(-50.0, 75.0)]
-
-model_per_variable = []
-for (i, v) in enumerate(bioclim_variables)
-    @info i
-    push!(model_per_variable, get_quantile_matrix(v, predicates[i], b_box, longitudes, latitudes))
+# Utility functions for GBIF + example data
+function Base.getindex(p::SDMLayer, r::GBIFRecord)
+    return p[r.longitude, r.latitude]
 end
 
-retained_models = filter(x -> sum(filter(.!isnan, x)) > 0.0, model_per_variable)
+sp = taxon("Cardinalis cardinalis")
+occ = occurrences(sp)
+next!(occ)
+function is_in_canada(r::GBIFRecord)
+    return r.country ∈ ["Canada", "United States"]
+end
+qualitycontrol!(occ; filters=[have_ok_coordinates, have_both_coordinates, is_in_canada])
 
-consensus = zeros(Float64, size(retained_models[1]))
-for i in eachindex(consensus)
-    consensus[i] = minimum(getindex.(retained_models, i))
+
+# Get the worldclim data by their layer number
+lay1 = worldclim(1)
+
+# Brick the layer by species observations
+lats = Float64[]
+lons = Float64[]
+for r in occ
+    push!(lats, r.latitude)
+    push!(lons, r.longitude)
+end
+lay1 = lay1[(minimum(lons)-1.0, maximum(lons)+1.0), (minimum(lats)-1.0, maximum(lats)+1.0)]
+
+heatmap(longitudes(lay1), latitudes(lay1), lay1.grid, aspectratio=1.3, c=:viridis)
+scatter!(lons, lats, c=:black, msw=0.0, ms=3, lab="")
+
+obs1 = zeros(Float64, count(occ.show))
+i = 1
+for r in occ
+    obs1[i] = lay1[r]
+    global i += 1
 end
 
-obs_lon = []
-obs_lat = []
-for r in occ_data
-    push!(obs_lon, r.longitude)
-    push!(obs_lat, r.latitude)
-end
-
-b_box_lon = range(b_box[1][1]; stop=b_box[2][1], length=size(consensus, 2))
-b_box_lat = range(b_box[1][2]; stop=b_box[2][2], length=size(consensus, 1))
-
-coordinates = []
-for r in occ_data
-    push!(coordinates, (r.longitude, r.latitude))
+function find_quantile(itr, p; ql=100)
+    qrange = range(0.0; stop=1.0, length=ql)
+    q = quantile(itr, qrange; sorted=true)
+    return qrange[findmin(abs.(q.-p))[2]]
 end
 
 
-raw_suit = [get_value_at_position(coordinate, consensus, collect(b_box_lon), collect(b_box_lat)) for coordinate in coordinates]
-suitability = filter(.!isnan, raw_suit)
-tr = quantile(suitability, [0.05])[1]
-
-p1 = heatmap(b_box_lon, b_box_lat, round.(consensus; digits=2), frame=:box, c=:viridis, clim=(0,1), leg=false, aspectratio=1.4)
-
-tokeep = zeros(Int64, size(consensus))
-for i in eachindex(tokeep)
-    if !isnan(consensus[i])
-        tokeep[i] = consensus[i] > tr ? 2 : 1
-    else
-        tokeep[i] = 0
-    end
-end
-
-p2 = heatmap(b_box_lon, b_box_lat, tokeep, frame=:box, c=:Blues, aspectratio=1.4, leg=false)
-scatter!(p2, obs_lon, obs_lat, leg=false, m=:cross, ms=1, c=:orange, msw=0.0)
-
-for p in (p1, p2)
-    xaxis!(p, "Longitude", (minimum(b_box_lon), maximum(b_box_lon)))
-    yaxis!(p, "Latitude", (minimum(b_box_lat), maximum(b_box_lat)))
-end
-
-plot(p1, p2)
