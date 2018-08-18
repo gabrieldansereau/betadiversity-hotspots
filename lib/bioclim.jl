625f94775c24e951641ee27476b4b38299ce5388
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-08-18 14:21:21 -0400

    faster code - change the example taxon

--- lib/bioclim.jl
+++ lib/bioclim.jl
@@ -1,17 +1,12 @@
-function find_quantile(itr, p; ql=150)
-    qrange = range(0.0; stop=1.0, length=ql)
-    q = quantile(itr, qrange; sorted=true)
-    return qrange[findmin(abs.(q.-p))[2]]
-end
-
 function bioclim(p::SDMLayer, r::GBIFRecords)
-    observed_values = sort(p[r])
+    observed_values = p[r]
+    qfinder = ecdf(observed_values)
     lq = zeros(Float64, size(p))
     for i in eachindex(p.grid)
         if isnan(p.grid[i])
             local_quantile = NaN
         else
-            local_quantile = find_quantile(observed_values, p.grid[i])
+            local_quantile = qfinder(p.grid[i])
             if local_quantile > 0.5
                 local_quantile = 1.0-local_quantile
             end
--- main.jl
+++ main.jl
@@ -1,7 +1,9 @@
 using Plots
-using Statistics
 using GDAL
+using Shapefile
 using GBIF
+using StatsBase
+using Statistics
 
 include("lib/SDMLayer.jl")
 include("lib/gdal.jl")
@@ -10,8 +12,8 @@ include("lib/bioclim.jl")
 
 # Get some GBIF data
 q = Dict{Any,Any}("country" => "CA", "limit" => 100)
-occ = occurrences(taxon("Lepus townsendii"), q)
-[next!(occ) for i in 1:50]
+occ = occurrences(taxon("Turdus migratorius"), q)
+[next!(occ) for i in 1:8]
 qualitycontrol!(occ; filters=[have_ok_coordinates, have_both_coordinates])
 
 # Get the worldclim data by their layer number
@@ -21,14 +23,50 @@ qualitycontrol!(occ; filters=[have_ok_coordinates, have_both_coordinates])
 # Make the final prediction by taking the minimum
 @time prediction = reduce(minimum, predictions);
 
-heatmap(
-        longitudes(prediction), latitudes(prediction), prediction.grid, 
-        aspectratio=1.3, c=:Greens
-       )
-savefig("sdm.png")
+# Filter the predictions so that everything below the 10th percentile is NaN
+threshold = quantile(prediction[occ], [0.1])[1]
+for i in eachindex(prediction.grid)
+    prediction.grid[i] < threshold && (prediction.grid[i] = NaN)
+end
+
+# Get the map and plot it
+function get_shape(res)
+    @assert res ∈ [50,110]
+    dir = "https://github.com/nvkelso/natural-earth-vector/raw/master/$(res)m_physical/"
+    fn = "ne_$(res)m_land.shp"
+    run(`wget $dir/$fn -P /tmp/`)
+    handle = open("/tmp/$fn", "r") do io
+        read(io, Shapefile.Handle)
+    end
+    return handle
+end
+lores = get_shape(50)
+
+sdm_plot = plot([0.0], lab="", msw=0.0, ms=0.0, size=(1200,600), frame=:box)
+xaxis!(sdm_plot, (prediction.left,prediction.right), "Longitude")
+yaxis!(sdm_plot, (prediction.bottom,prediction.top), "Latitude")
+
+for p in lores.shapes
+    xy = map(x -> (x.x, x.y), p.points)
+    sh = Shape([pp.x for pp in p.points], [pp.y for pp in p.points])
+    plot!(sdm_plot, sh, c=:lightgrey, lab="")
+end
+
+heatmap!(
+    sdm_plot,
+    longitudes(prediction), latitudes(prediction), prediction.grid, 
+    aspectratio=1.3, c=:Oranges, frame=:box
+    )
+
+for p in lores.shapes
+    xy = map(x -> (x.x, x.y), p.points)
+    plot!(sdm_plot, xy, c=:grey, lab="")
+end
 
 scatter!(
-         longitudes(occ), latitudes(occ),
-         c=:black, msw=0.0, ms=1, lab=""
-        )
-savefig("sdm_occ.png")
+    sdm_plot,
+    longitudes(occ), latitudes(occ),
+    c=:black, msw=0.0, ms=0.5, lab=""
+    )
+
+savefig("sdm.png")
