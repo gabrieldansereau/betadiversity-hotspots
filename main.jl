74bce91c1dc9774d5699cc63fea8dbf9d8dd18c0
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-08-19 10:38:59 -0400

    update GBIF to fix iteration bug, add some @info when running

--- main.jl
+++ main.jl
@@ -13,30 +13,35 @@ include("lib/shapefiles.jl")
 
 # Get some GBIF data
 q = Dict{Any,Any}("limit" => 200)
-occ = occurrences(taxon("Cardinalis cardinalis"), q)
-[next!(occ) for i in 1:49]
+occ = occurrences(taxon("Cypripedium reginae"), q)
+[next!(occ) for i in 1:19]
 function is_ca_or_us(r::GBIFRecord)
     r.countryCode ∈ ["CA", "US"]
 end
 qualitycontrol!(occ; filters=[have_ok_coordinates, have_both_coordinates, is_ca_or_us])
 
 # Get the worldclim data by their layer number
+@info "Extract and crop bioclim variables"
 @time wc_vars = [clip(worldclim(i), occ) for i in 1:19];
 # Make the prediction for each layer
+@info "Predictions for each layer"
 @time predictions = [bioclim(wc_vars[i], occ) for i in 1:length(wc_vars)];
 # Make the final prediction by taking the minimum
+@info "Minimum-consensus aggregation"
 @time prediction = reduce(minimum, predictions);
 # Get the threshold for NaN given a percentile
+@info "Threshold estimation"
 @time threshold = first(quantile(prediction[occ], [0.05]))
+@info "5% threshold:\t$(round(threshold; digits=3))"
 # Filter the predictions based on the threshold
+@info "Final prediction filtering"
 @time for i in eachindex(prediction.grid)
     prediction.grid[i] < threshold && (prediction.grid[i] = NaN)
 end
 
-
 worldmap = clip(worldshape(50), prediction)
 
-sdm_plot = plot([0.0], lab="", msw=0.0, ms=0.0, size=(1200,600), frame=:box)
+sdm_plot = plot([0.0], lab="", msw=0.0, ms=0.0, size=(900,450), frame=:box)
 xaxis!(sdm_plot, (prediction.left,prediction.right), "Longitude")
 yaxis!(sdm_plot, (prediction.bottom,prediction.top), "Latitude")
 
@@ -48,7 +53,7 @@ end
 heatmap!(
     sdm_plot,
     longitudes(prediction), latitudes(prediction), prediction.grid, 
-    aspectratio=1.3, c=:viridis,
+    aspectratio=1.3, c=:BuPu,
     clim=(0.0, maximum(filter(!isnan, prediction.grid)))
     )
 
