a2636a70b91935861982dab51c9288c0bdba9213
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-07-25 18:23:20 -0400

    add script to load required packages & functions

---
+++ src/required.jl
@@ -0,0 +1,15 @@
+using Plots
+using GDAL
+using Shapefile
+using GBIF
+using StatsBase
+using Statistics
+using DataFrames
+using CSV
+
+include("lib/SDMLayer.jl")
+include("lib/gdal.jl")
+include("lib/worldclim.jl")
+include("lib/bioclim.jl")
+include("lib/shapefiles.jl")
+include("lib/csvdata.jl")
