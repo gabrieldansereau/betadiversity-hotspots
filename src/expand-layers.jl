3d1d7d2b13e092105bf0338845fcb88f70871e78
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-08-22 16:35:34 -0400

    update expand_layers function

--- src/expand-layers.jl
+++ src/expand-layers.jl
@@ -24,62 +24,76 @@ temp = wc_vars[1]
 ## Load predictions
 @load "../data/predictions-ebd.jld2" predictions
 pred = predictions[1]
+@load "../data/predictions-can.jld2" predictions
+pred2 = predictions[1]
 
-## expand
+## Create expand_layers function
+
+#=
 p1 = temp
 p2 = pred
-
-# Get minimum coordinates
-#=
-min_lon = min(p1.left, p2.left)
-max_lon = max(p1.right, p2.right)
-min_lat = min(p1.bottom, p2.bottom)
-max_lat = max(p1.top, p2.top)
 =#
 
-# Get coordinates
-lons_p1 = longitudes(p1)
-lats_p1 = latitudes(p1)
-lons_p2 = longitudes(p2)
-lats_p2 = latitudes(p2)
+function expand_layers(p1::SDMLayer, p2::SDMLayer)
+    layers = [p1, p2]
 
-# Get position of p2 of p1
-m_lon = findmin(abs.(minimum(lons_p2) .- lons_p1))[2]
-M_lon = findmin(abs.(maximum(lons_p2) .- lons_p1))[2]
-m_lat = findmin(abs.(minimum(lats_p2) .- lats_p1))[2]
-M_lat = findmin(abs.(maximum(lats_p2) .- lats_p1))[2]
-p1[(m_lat:M_lat), (m_lon:M_lon)]
-# Check is size matches
-size(p2)
+    # Get minimum coordinates
+    min_lon = min(map(x -> x.left, layers)...)
+    max_lon = max(map(x -> x.right, layers)...)
+    min_lat = min(map(x -> x.bottom, layers)...)
+    max_lat = max(map(x -> x.top, layers)...)
 
-# Create new layer
-new1 = fill(NaN, size(p1))
-new2 = fill(NaN, size(p1))
-new1[(m_lat:M_lat), (m_lon:M_lon)] .= p1[(m_lat:M_lat), (m_lon:M_lon)]
-new2[(m_lat:M_lat), (m_lon:M_lon)] .= p2.grid
-newlayer1 = SDMLayer(new1, p1.left, p1.right, p1.bottom, p1.top)
-newlayer2 = SDMLayer(new2, p1.left, p1.right, p1.bottom, p1.top)
+    # Get grid size (rounding should prevent problems with last decimal)
+    grid_size_lons = 1/round(1/stride(layers[1],1))
+    grid_size_lats = 1/round(1/stride(layers[1],2))
+
+    # Get coordinate range of newlayer -> original layers must have same stride
+    lons_newlayers = min_lon+grid_size_lons:2*grid_size_lons:max_lon-grid_size_lons
+    longitudes(p1)
+    lats_newlayers = min_lat+grid_size_lats:2*grid_size_lats:max_lat-grid_size_lats
+    latitudes(p1)
+
+    # Create expanded layers
+    newlayers = []
+    for i in 1:length(layers)
+        # Get position of original layers in new layer
+        m_lon = findmin(abs.(layers[i].left+grid_size_lons .- lons_newlayers))[2]
+        M_lon = findmin(abs.(layers[i].right-grid_size_lons .- lons_newlayers))[2]
+        m_lat = findmin(abs.(layers[i].bottom+grid_size_lats .- lats_newlayers))[2]
+        M_lat = findmin(abs.(layers[i].top-grid_size_lats .- lats_newlayers))[2]
+        # Create new grid
+        newgrid = fill(NaN, length(lats_newlayers), length(lons_newlayers))
+        # Fill in original values
+        newgrid[(m_lat:M_lat), (m_lon:M_lon)] .= layers[i].grid
+        # Convert to SDMLayer
+        newlayer = SDMLayer(newgrid, min_lon, max_lon, min_lat, max_lat)
+        # Export result
+        push!(newlayers, newlayer)
+    end
+    return newlayers
+end
+
+@time newlayers = expand_layers(pred, pred2)
 
 # Plot result
-plotp1 = plotSDM(p1)
-plotp2 = plotSDM(p2)
-plotn1 = plotSDM(newlayer1)
-plotn2 = plotSDM(newlayer2)
+plotp1 = plotSDM(temp)
+plotp2 = plotSDM(pred)
+plotn1 = plotSDM(newlayers[1])
+plotn2 = plotSDM(newlayers[2])
 
 # Combine heatmaps
-sdm_plot = plotSDM(newlayer1)
+sdm_plot = plotSDM(newlayers[1])
 heatmap!(
     sdm_plot,
-    longitudes(newlayer2), latitudes(newlayer2), # layer range
-    newlayer2.grid, # evenness values
-    aspectratio=92.60/60.75, # aspect ratio
+    longitudes(newlayers[2]), latitudes(newlayers[2]), # layer range
+    newlayers[2].grid, # evenness values
     c=:viridis, # ~color palette
-    clim=(0.0, maximum(filter(!isnan, newlayer2.grid))) # colorbar limits
+    clim=(0.0, maximum(filter(!isnan, newlayers[2].grid))) # colorbar limits
 )
 plot!(
     sdm_plot,
-    xlims=(p2.left, p2.right),
-    ylims=(p2.bottom, p2.top),
+    xlims=(pred.left, pred.right),
+    ylims=(pred.bottom, pred.top),
     aspectratio=92.60/60.75
 )
 # Compare to p2
