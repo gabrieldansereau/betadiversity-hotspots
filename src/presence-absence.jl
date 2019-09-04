059c1750ded132febbd1c49d0004bfe9c107db6a
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-09-04 14:34:09 -0400

    explore LCBD on raw data

--- src/presence-absence.jl
+++ src/presence-absence.jl
@@ -1,5 +1,6 @@
 using Distributed
 using JLD2
+addprocs(4)
 
 @time @everywhere include("src/required.jl")
 
@@ -20,43 +21,84 @@ end
 ## Get the worldclim data
 @time wc_vars = pmap(x -> worldclim(x)[lon_range, lat_range], 1:19);
 
-## Create new layer
-pres_abs = SDMLayer{Float64}[]
-@time for species in warblers_occ
+## Create function to convert occurrence to presence-absence based on a SDMLayer
+@everywhere function presence_absence(species::DataFrame, copy_layer::SDMLayer; binary::Bool=true)
     # Create empty grid for presence-absence data (with NaN)
-    pres_abs_grid = copy(wc_vars[1].grid)
+    pres_abs_grid = copy(copy_layer.grid)
     replace!(x -> !isnan(x) ? 0.0 : x, pres_abs_grid)
-    # Reduce to unique sites/coordinates
+    # Get unique sites/coordinates
     unique_sites = unique(species, [:longitude, :latitude])
     # Loop for all sites
     for site in eachrow(unique_sites)
         # Get grid position
-        i_lon = findmin(abs.(site.longitude .- longitudes(wc_vars[1])))[2]
-        j_lat = findmin(abs.(site.latitude .- latitudes(wc_vars[1])))[2]
+        i_lon = findmin(abs.(site.longitude .- longitudes(copy_layer)))[2]
+        j_lat = findmin(abs.(site.latitude .- latitudes(copy_layer)))[2]
         # Add 1 per species presence
         pres_abs_grid[j_lat, i_lon] += 1.0
     end
     # Check sites with presence
-    filter(!iszero, pres_abs_grid)
+    filter(x -> x > 0.0, pres_abs_grid)
     # Reduce sites with more than 1 presence
-    replace!(x -> x > 1.0 ? 1.0 : x, pres_abs_grid)
+    if binary == true
+        replace!(x -> x > 1.0 ? 1.0 : x, pres_abs_grid)
+    end
     # Create SDMLayer
     pres_abs_layer = SDMLayer(pres_abs_grid,
-                                wc_vars[1].left, wc_vars[1].right,
-                                wc_vars[1].bottom, wc_vars[1].top)
-    # Export result
-    push!(pres_abs, pres_abs_layer)
+                              copy_layer.left, copy_layer.right,
+                              copy_layer.bottom, copy_layer.top)
+    return pres_abs_layer
 end
+# Loop function for each species
+@time pres_abs = pmap(x -> presence_absence(x, wc_vars[1]), warblers_occ)
+# @time pres_abs1 = pmap(x -> presence_absence(x, wc_vars[1], binary=false), warblers_occ)
+
+# Count sites with presence per species
+pres_counts = [length(filter(x -> x > 0.0, species.grid)) for species in pres_abs]
+sort(pres_counts) # one with 0 sites
+
+## Plot result
+plotSDM(pres_abs[1])
 
 # Get dimensions
-nsites = prod(size(predictions[1]))
-nspecies = length(predictions)
+nsites = prod(size(pres_abs[1]))
+nspecies = length(pres_abs)
 # Create Y
 Y = zeros(Int64, (nsites, nspecies))
 # Fill Y with community predictions
-@progress for gc in eachindex(predictions[1].grid) # loop for all sites
+@progress for gc in 1:nsites # loop for all sites
     # Group predictions for all species in site
-    R = map(x -> x.grid[gc], predictions)
-    # Fill Y with binary values -> 1 if species prediction for site != NaN, 0 if == NaN
-    global Y[gc,:] = .!isnan.(R)
+    R = map(x -> x.grid[gc], pres_abs)
+    # Fill Y with binary values
+    global Y[gc,:] = isone.(R)
 end
+
+## Compute beta diversity statistics
+# Load functions
+include("lib/beta-div.jl")
+## Option 2: Calculate LCBD only for sites with predictions
+# Get index of sites with predictions
+sites_pred = map(x -> any(x .> 0), eachrow(Y))
+inds_pred = findall(sites_pred)
+# Select sites with predictions only
+Ypred = Y[inds_pred,:]
+# Compute BD statistics
+resBDpred = BD(Ypred)
+# Extract LCBD values
+LCBDi = resBDpred.LCBDi
+# Scale LCBDi values to maximum value
+LCBDi = LCBDi./maximum(LCBDi)
+
+## Arrange LCBD values as grid
+# Create empty grid
+t_lcbd = fill(NaN, size(pres_abs[1]))
+# Fill in grid
+t_lcbd[inds_pred] = LCBDi
+# Create SDMLayer with LCBD values
+LCBD = SDMLayer(t_lcbd, pres_abs[1].left, pres_abs[1].right, pres_abs[1].bottom, pres_abs[1].top)
+
+## Plot results
+lcbd_plot = plotSDM(LCBD, type="lcbd")
+title!(lcbd_plot, "LCBD values per site (relative to maximum)")
+
+## Save result
+# savefig(lcbd_plot, "fig/pres-abs-ebd.pdf")
