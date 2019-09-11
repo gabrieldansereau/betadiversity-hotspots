498dd2e762ca5d3474fc6230e7f61b0a4161754a
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-09-11 19:28:56 -0400

    add permutation tests to lcbd script

--- src/raw-lcbd-warblers.jl
+++ src/raw-lcbd-warblers.jl
@@ -1,5 +1,6 @@
 using Distributed
 using JLD2
+using Random
 @time include("required.jl")
 
 ## Load presence-absence data for all species
@@ -10,36 +11,53 @@ using JLD2
 ## Compute beta diversity statistics
 # Load functions
 include("lib/beta-div.jl")
-## Option 2: Calculate LCBD only for sites with predictions
-# Compute BD statistics
-resBDpred = BD(Ypred)
+# Compute BD statistics on raw data
+resBDpres = BD(Ypred)
+# Compute BD statistics on transformed data
 resBDtransf = BD(Ytransf)
+#=
+# Run permutation tests on transformed data
+@time resBDperm = BDperm(Ytransf, nperm = 999, distributed = false) # 1600 sec/27 min for 999 permutations
+# Export permutation results
+@save "../data/raw-resBDperm-transf.jld2" resBDperm
+=#
+# Load permutation results
+@load "../data/raw-resBDperm-transf.jld2" resBDperm
+
 # Extract LCBD values
-LCBDi = resBDpred.LCBDi
-LCBDi_tr = resBDtransf.LCBDi
+resBD = [resBDpres, resBDtransf, resBDperm]
+LCBDsets = [res.LCBDi for res in resBD]
 # Scale LCBDi values to maximum value
-LCBDi = LCBDi./maximum(LCBDi)
-LCBDi_tr = LCBDi_tr./maximum(LCBDi_tr)
+LCBDsets = [LCBDi./maximum(LCBDi) for LCBDi in LCBDsets]
 
 ## Arrange LCBD values as grid
-# Create empty grid
-t_lcbd = fill(NaN, size(pres_abs[1]))
-t_lcbd_tr = fill(NaN, size(pres_abs[1]))
-# Fill in grid
-t_lcbd[inds_pred] = LCBDi
-t_lcbd_tr[inds_pred] = LCBDi_tr
+# Create empty grids
+t_lcbd = [fill(NaN, size(pres_abs[1])) for LCBDi in LCBDsets]
+# Fill in grid for resBDpres & resBDperm
+t_lcbd[1][inds_pred] = LCBDsets[1]
+t_lcbd[2][inds_pred] = LCBDsets[2]
+# Get indices of sites with significant LCBDs
+inds_signif = falses(size(Y,1))
+inds_signif[inds_pred] = Array{Bool}(resBD[3].pLCBD .<= 0.05)
+# Get indices of sites with significant LCBDs & predictions
+inds_signifpred = intersect(findall(inds_signif), inds_pred)
+# Fill in grid for resBDpred
+t_lcbd[3][inds_pred] .= 0.0
+t_lcbd[3][inds_signifpred[3]] .= 1.0
 # Create SDMLayer with LCBD values
-LCBD = SDMLayer(t_lcbd, pres_abs[1].left, pres_abs[1].right, pres_abs[1].bottom, pres_abs[1].top)
-LCBDtr = SDMLayer(t_lcbd_tr, pres_abs[1].left, pres_abs[1].right, pres_abs[1].bottom, pres_abs[1].top)
+LCBD = SDMLayer.(t_lcbd, pres_abs[1].left, pres_abs[1].right, pres_abs[1].bottom, pres_abs[1].top)
 
 ## Plot results
-lcbd_plot = plotSDM(LCBD, type="lcbd")
-title!(lcbd_plot, "LCBD values per site (relative to maximum, raw data)")
-lcbdtr_plot = plotSDM(LCBDtr, type="lcbd")
-title!(lcbdtr_plot, "LCBD values per site (relative to maximum, hellinger transformed)")
+lcbd_plot1 = plotSDM(LCBD[1], type="lcbd")
+title!(lcbd_plot1, "LCBD values per site (relative to maximum, raw data)")
+lcbd_plot2 = plotSDM(LCBD[2], type="lcbd")
+title!(lcbd_plot2, "LCBD values per site (relative to maximum, hellinger transformed)")
+lcbd_plot3 = plotSDM(LCBD[3], type="lcbd")
+title!(lcbd_plot3, "Significant LCBDs (hellinger transformed)")
 
 ## Save result
 #=
-savefig(lcbd_plot, "fig/raw-lcbd.pdf")
-savefig(lcbdtr_plot, "fig/raw-lcbd-transf.pdf")
+savefig(lcbd_plot1, "fig/raw-lcbd.pdf")
+savefig(lcbd_plot2, "fig/raw-lcbd-transf.pdf")
+savefig(lcbd_plot3, "fig/raw-lcbd-signif.pdf")
 =#
