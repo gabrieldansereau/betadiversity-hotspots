63b4beaa4a5fd65c2dfff005ba23b531d792f2f3
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-09-11 23:47:04 -0400

    minor fix

--- src/raw-relation-lcbd-richness.jl
+++ src/raw-relation-lcbd-richness.jl
@@ -14,13 +14,13 @@ using JLD2
 # Calculate relative richness (α/γ)
 rel_richness = richness.grid ./ size(Y, 2)
 # Scatterplot LCBD ~ richness
-relation_plot = scatter(vec(rel_richness), vec(LCBD.grid),
+relation_plot = scatter(vec(rel_richness), vec(LCBD[1].grid),
         markersize = 1,
         yticks = 0.0:0.20:1.0,
         title = "Relationship between LCBD and species richness",
         xlabel = "Species richness (\\alpha\\/\\gamma)", ylabel = "LCBD (relative to maximum)",
         legend = :none, grid=:none)
-relationtr_plot = scatter(vec(rel_richness), vec(LCBDtr.grid),
+relationtr_plot = scatter(vec(rel_richness), vec(LCBD[2].grid),
         markersize = 1,
         yticks = 0.0:0.20:1.0,
         title = "Relationship between LCBD (hellinger transformed) and species richness",
