6c6a6d32821fafd9ea89add909d65ed323b1cdce
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-09-11 20:52:01 -0400

    add separate LCBD-richness relationship script

---
+++ src/raw-relation-lcbd-richness.jl
@@ -0,0 +1,34 @@
+using Distributed
+using JLD2
+@time include("required.jl")
+
+## Load LCBD & richness scripts (if not already loaded)
+#=
+# Load richness script
+@time include("raw-richness-warblers.jl")
+# Load LCBD script
+@time include("raw-lcbd-warblers.jl")
+=#
+
+## Richness-LCBD relationship
+# Calculate relative richness (α/γ)
+rel_richness = richness.grid ./ size(Y, 2)
+# Scatterplot LCBD ~ richness
+relation_plot = scatter(vec(rel_richness), vec(LCBD.grid),
+        markersize = 1,
+        yticks = 0.0:0.20:1.0,
+        title = "Relationship between LCBD and species richness",
+        xlabel = "Species richness (\\alpha\\/\\gamma)", ylabel = "LCBD (relative to maximum)",
+        legend = :none, grid=:none)
+relationtr_plot = scatter(vec(rel_richness), vec(LCBDtr.grid),
+        markersize = 1,
+        yticks = 0.0:0.20:1.0,
+        title = "Relationship between LCBD (hellinger transformed) and species richness",
+        xlabel = "Species richness (\\alpha\\/\\gamma)", ylabel = "LCBD (relative to maximum)",
+        legend = :none, grid=:none)
+
+## Save result
+#=
+savefig(relation_plot, "fig/raw-relation-lcbd-richness.png")
+savefig(relationtr_plot, "fig/raw-relation-lcbd-richness-transf.png")
+=#
