ab2718b8b0c7c6b55f8d66e7dd03be5cda2cb6a7
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-09-19 15:33:46 -0400

    remove lib/ scripts useless in SimpleSDMLayers

--- src/lib/worldclim.jl
+++
@@ -1,6 +0,0 @@
-function worldclim(i::Int64; resolution="5", path="assets")
-    @assert 1 ≤ i ≤ 19
-    code = lpad(i, 2, "0")
-    raw_data = get_data_from_tiff(joinpath(path, "wc2.0_bio_$(resolution)m_$(code).tif"))
-    return SimpleSDMPredictor(raw_data, -180.0, 180.0, -90.0, 90.0)
-end
