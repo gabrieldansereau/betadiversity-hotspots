c725335efabf4eda93f5e5d52749b6f063196d9a
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-09-20 17:14:49 -0400

    train bioclim with finer resolution

--- src/lib/bioclim.jl
+++ src/lib/bioclim.jl
@@ -1,7 +1,7 @@
 import SimpleSDMLayers: bioclim
 
-function bioclim(p::SimpleSDMLayer, r::Union{GBIFRecords,DataFrame})
-    observed_values = p[r]
+function bioclim(p::SimpleSDMLayer, r::Union{GBIFRecords,DataFrame}; train::SimpleSDMLayer=p)
+    observed_values = train[r]
     qfinder = ecdf(observed_values)
     lq = zeros(Float64, size(p))
     for i in eachindex(p.grid)
@@ -18,29 +18,8 @@ function bioclim(p::SimpleSDMLayer, r::Union{GBIFRecords,DataFrame})
     end
     prediction = SimpleSDMResponse(lq, p.left, p.right, p.bottom, p.top)
 end
-function bioclim_training(t::SimpleSDMLayer, r::Union{GBIFRecords,DataFrame})
-    observed_values = t[r]
-    qfinder = ecdf(observed_values)
-    return qfinder
-end
-function bioclim_prediction(p::SimpleSDMLayer, r::Union{GBIFRecords,DataFrame}, qfinder::ECDF{Array{Float64,1}})
-    lq = zeros(Float64, size(p))
-    for i in eachindex(p.grid)
-        if isnan(p.grid[i])
-            local_quantile = NaN
-        else
-            local_quantile = qfinder(p.grid[i])
-            if local_quantile > 0.5
-                local_quantile = 1.0-local_quantile
-            end
-            local_quantile = 2.0 * local_quantile
-        end
-        lq[i] = local_quantile
-    end
-    prediction = SimpleSDMResponse(lq, p.left, p.right, p.bottom, p.top)
-end
-function species_bclim(occ, vars)
-    predictions = [bioclim(vars[i], occ) for i in 1:length(vars)];
+function species_bclim(occ, vars; train=vars)
+    predictions = [bioclim(vars[i], occ, train = train[i]) for i in 1:length(vars)];
     prediction = reduce(minimum, predictions);
     for i in eachindex(prediction.grid)
         if prediction.grid[i] == 0.0
