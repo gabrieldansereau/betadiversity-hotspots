ad5c7372ac051076d184783eafd1e806bd6d0f2d
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-12-17 19:15:09 -0500

    :sparkles: explore ways to scale LCBD results

--- src/sdm/05_sdm_lcbd.jl
+++ src/sdm/05_sdm_lcbd.jl
@@ -22,15 +22,15 @@ resBDprob = BD(Yprob)
 resBD = [resBDpred, resBDtransf, resBDprob]
 LCBDsets = [res.LCBDi for res in resBD]
 # Scale LCBDi values to maximum value
+LCBDsets_raw = copy(LCBDsets)
 LCBDsets = [LCBDi./maximum(LCBDi) for LCBDi in LCBDsets]
+LCBDsets = [LCBDsets..., LCBDsets_raw...]
 
 ## Arrange LCBD values as grid
 # Create empty grids
 t_lcbd = [fill(NaN, size(predictions[1])) for LCBDi in LCBDsets]
 # Fill in grid for resBDpred & resBDperm
-t_lcbd[1][inds_pred] = LCBDsets[1]
-t_lcbd[2][inds_pred] = LCBDsets[2]
-t_lcbd[3][inds_pred] = LCBDsets[3]
+[t_lcbd[i][inds_pred] = LCBDsets[i] for i in 1:length(t_lcbd)]
 # Create SimpleSDMLayer with LCBD values
 LCBD = SimpleSDMResponse.(t_lcbd, predictions[1].left, predictions[1].right, predictions[1].bottom, predictions[1].top)
 
@@ -47,4 +47,52 @@ title!(lcbd_plot3, "SDM LCBD values per site (relative to maximum, probability d
 savefig(lcbd_plot1, "fig/sdm/05_sdm_lcbd.pdf")
 savefig(lcbd_plot2, "fig/sdm/05_sdm_lcbd-transf.pdf")
 savefig(lcbd_plot3, "fig/sdm/05_sdm_lcbd-prob.pdf")
+=#
+
+
+## Investigate different ways to scale LCBD results
+
+#=
+# LCBD values in 10 quantiles
+n = 10
+lcbd = copy(LCBD[1])
+lcbd_nonan = filter(!isnan, lcbd.grid)
+q = quantile(lcbd_nonan, 0.0:1/n:1.0)
+for i in length(q)-1:-1:1
+    replace!(x -> q[i] <= x <= q[i+1] ? i : x, lcbd.grid)
+end
+by(DataFrame(lcbd = vec(lcbd.grid)), :lcbd, nrow)
+plotSDM(lcbd, c=:viridis)
+plotSDM(lcbd, c=:magma)
+
+# LCBD values by ecdf distribution
+lcbd_ecdf = copy(LCBD[1])
+qfinder = ecdf(filter(!isnan, lcbd_ecdf.grid))
+replace!(x -> !isnan(x) ? qfinder(x) : x, lcbd_ecdf.grid)
+lcbd_ecdf_viridis = plotSDM(lcbd_ecdf, c=:viridis)
+lcbd_ecdf_reds = plotSDM(lcbd_ecdf, c=:reds)
+
+
+# Raw LCBD scores
+lcbd_raw = copy(LCBD[4])
+lcbd_raw_plot = plotSDM(lcbd_raw, c=:viridis)
+lcbd_raw_z = copy(LCBD[4])
+lcbd_raw_nonan = filter(!isnan, lcbd_std.grid)
+# LCBD zscores
+raw_zscores = zscore(lcbd_raw_nonan)
+lcbd_raw_z.grid[inds_pred] = raw_zscores
+lcbd_raw_z_plot = plotSDM(lcbd_raw_z, c=:viridis)
+heatmap!(lcbd_raw_z_plot, clim=(0-maximum(raw_zscores), maximum(raw_zscores)))
+# SSi instead of LCBD
+lcbd_ssi = copy(LCBD[4])
+lcbd_ssi.grid[inds_pred] = resBDpred.SSi
+plotSDM(lcbd_ssi, c=:viridis)
+
+# Plot few results
+lcbd_plot1
+lcbd_ecdf_viridis
+lcbd_ecdf_reds
+richness_plot
+richness_viridis
+richness_reds
 =#
