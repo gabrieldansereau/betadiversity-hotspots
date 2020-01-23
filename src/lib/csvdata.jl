1b1cbae0143224917fa570fc5ec2668c0d83740d
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-01-23 13:55:27 -0500

    :sparkles: create function to load landcover from ArchGDAL

--- src/lib/csvdata.jl
+++ src/lib/csvdata.jl
@@ -33,23 +33,3 @@ function prepare_ebd_data(df::DataFrame)
     filter!(obs -> obs[:approved] .== 1, df)
     return df
 end
-
-function load_landcover(lon_range, lat_range;path::AbstractString="assets/landcover/")
-    # Load variable names & paths
-    landcover_files = readdir(path)
-    landcover_vars = [split(lc, r"_")[2] for lc in landcover_files]
-    landcover_paths = string.(path, landcover_files)
-
-    # Load data
-    landcover_raw = readdlm.(landcover_paths)
-    # Reverse rows
-    landcover_mat = [lc[end:-1:1, :] for lc in landcover_raw]
-
-    # Replace 255 (default no data values) by NaN
-    [replace!(lc, 255 => NaN) for lc in landcover_mat]
-    # Add additionnal line of NaNs down South (coordinates prime did not exactly match)
-    landcover_mat = [vcat(fill(NaN, (1, size(lc, 2))), lc) for lc in landcover_mat]
-
-    # Convert to SDMLayers, cut to coordinates range
-    landcover_layers = [SimpleSDMPredictor(lc, -160.0, -40.0, 20.0, 80.0)[lon_range, lat_range] for lc in landcover_mat]
-end
