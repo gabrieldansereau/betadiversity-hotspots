b65a9c72f2daacb27726ffeb63f5a0678522d2b7
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-02-06 09:29:45 -0500

    :sparkles: create mutating quantiles! function

--- src/lib/quantiles.jl
+++ src/lib/quantiles.jl
@@ -1,13 +1,38 @@
-# Function to extract quantile scores of a vector
-function quantiles(vec::Array{Float64,1})
-    ecdf_finder = ecdf(vec)
-    return ecdf_finder(vec)
+# Convert vector values to quantile scores
+function quantiles(vec::Array{Float64,1}; ignorenan::Bool=true)
+    if ignorenan == true
+        qfinder = ecdf(filter(!isnan, vec))
+        qvec = replace(x -> !isnan(x) ? qfinder(x) : x, vec)
+    else
+        qfinder = ecdf(vec)
+        qvec = qfinder(vec)
+    end
+    return qvec
 end
 
-# Function to extract quantile scores of a SimpleSDMLayer
+# Convert & mutate vector values to quantile scores
+function quantiles!(vec::Array{Float64,1}; ignorenan::Bool=true)
+    if ignorenan == true
+        qfinder = ecdf(filter(!isnan, vec))
+        replace!(x -> !isnan(x) ? qfinder(x) : x, vec)
+    else
+        qfinder = ecdf(vec)
+        replace!(x -> qfinder(x), vec)
+    end
+    return vec
+end
+
+# Convert SimpleSDMLayer values to quantile scores
 function quantiles(layer::SimpleSDMLayer)
-    ecdf_finder = ecdf(filter(!isnan, layer.grid))
-    qgrid = replace(x -> !isnan(x) ? ecdf_finder(x) : x, layer.grid)
+    qfinder = ecdf(filter(!isnan, layer.grid))
+    qgrid = replace(x -> !isnan(x) ? qfinder(x) : x, layer.grid)
     qlayer = SimpleSDMResponse(qgrid, layer.left, layer.right, layer.bottom, layer.top)
     return qlayer
+end
+
+# Convert & mutate SimpleSDMLayer values to quantile scores
+function quantiles!(layer::SimpleSDMLayer)
+    qfinder = ecdf(filter(!isnan, layer.grid))
+    replace!(x -> !isnan(x) ? qfinder(x) : x, layer.grid)
+    return layer
 end
