bd47700774c7cc0bd417df282e2e9581e4b7695d
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-02-19 15:32:00 -0500

    :sparkles: apply random forests

--- src/08_random-forests.jl
+++ src/08_random-forests.jl
@@ -2,8 +2,29 @@ import Pkg; Pkg.activate(".")
 using Distributed
 @time @everywhere include("src/required.jl")
 
-## Conditional arguments
-outcome = "raw" # desired outcome
+## Load data
+spe = CSV.read("data/proc/distributions_spe.csv", header=true, delim="\t")
+spa = CSV.read("data/proc/distributions_spa.csv", header=true, delim="\t")
+env = CSV.read("data/proc/distributions_env.csv", header=true, delim="\t")
 
-## Load presence-absence data for all species
-@load "data/jld2/$(outcome)-distributions.jld2" distributions spenames speindex
+## Perform RandomForests
+using RCall
+@rput spe spa env
+begin
+    R"""
+    library(party)
+    set.seed(42)
+    vars <- cbind(env, spa)
+    rf <- cforest(spe$sp1 ~ .,
+                  data = vars,
+                  control = cforest_unbiased(mtry = 2, ntree = 50))
+
+    set.seed(42)
+    vars_imp <- varimp(rf, conditional = T)
+
+    set.seed(42)
+    sp1_pred <- predict(rf, OOB=TRUE)
+
+    table(spe$sp1, sp1_pred)
+    """
+end
