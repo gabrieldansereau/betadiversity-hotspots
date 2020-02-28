512c3384215d8bc8bf6d32b9c6e06b39d687cc5d
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-02-28 16:21:58 -0500

    :construction: work on random forests

--- src/08_random-forests.R
+++ src/08_random-forests.R
@@ -1,19 +1,20 @@
 library(party)
 
-spe <-  read.csv("data/proc/distributions_spe.csv", header=TRUE, sep="\t")
-spa <-  read.csv("data/proc/distributions_spa.csv", header=TRUE, sep="\t")
-env <-  read.csv("data/proc/distributions_env.csv", header=TRUE, sep="\t")
+spe <-  read.csv("data/proc/distributions_spe_qc.csv", header=TRUE, sep="\t")
+spa <-  read.csv("data/proc/distributions_spa_qc.csv", header=TRUE, sep="\t")
+env <-  read.csv("data/proc/distributions_env_qc.csv", header=TRUE, sep="\t")
 
 head(spe)
 
 set.seed(42)
 vars <- cbind(env, spa)
-rf <- cforest(spe$sp1 ~ .,
+rf <- cforest(as.factor(spe$sp20) ~ .,
               data = vars,
-              control = cforest_unbiased(mtry = 2, ntree = 50))
+              control = cforest_unbiased(mtry = 2, ntree = 100))
 
 set.seed(42)
-vars_imp <- varimp(rf, conditional = T)
+(vars_imp <- varimp(rf, conditional = T))
+(vars_imp <- varimp(rf))
 
 set.seed(42)
 sp1_pred <- predict(rf, OOB=TRUE)
