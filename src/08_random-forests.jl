036e93d589afe1a0af48ae947eb6e5772a31b6be
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-03-02 16:05:48 -0500

    :rocket: predict full range distributions with RF

--- src/08_random-forests.jl
+++ src/08_random-forests.jl
@@ -12,19 +12,42 @@ using RCall
 @rput spe spa env
 begin
     R"""
-    library(party)
-    set.seed(42)
-    vars <- cbind(env, spa)
-    rf <- cforest(spe$sp1 ~ .,
-                  data = vars,
-                  control = cforest_unbiased(mtry = 2, ntree = 50))
+    library(randomForest)
 
-    set.seed(42)
-    vars_imp <- varimp(rf, conditional = T)
+    ## Predict distributions for full range
+    env_full <- read.csv("data/proc/distributions_env_full.csv", header = TRUE, sep = "\t")
+    spa_full <- read.csv("data/proc/distributions_spa_full.csv", header = TRUE, sep = "\t")
+    vars_full <- cbind(env_full, spa_full)
+    head(vars_full)
 
-    set.seed(42)
-    sp1_pred <- predict(rf, OOB=TRUE)
+    # Load model
+    load("data/proc/rf_models.RData")
 
-    table(spe$sp1, sp1_pred)
+    # Make predictions
+    system.time(rf_pred <- sapply(rf_models, function(x) predict(x, vars_full, type = "class")))
     """
 end
+@rget rf_pred
+
+Yrf = replace(rf_pred, missing => NaN,
+                           "0" => 0,
+                           "1" => 1)
+Yrf = Array{Float64}(Yrf)
+
+## Load distributions for all species
+@load "data/jld2/raw-distributions.jld2" distributions spenames speindex
+raw = (distributions = distributions,
+       spenames = spenames,
+       speindex = speindex)
+@load "data/jld2/sdm-distributions.jld2" distributions spenames speindex
+sdm = (distributions = distributions,
+       spenames = spenames,
+       speindex = speindex)
+
+rf_grids = [reshape(rf_pred[:,i], size(sdm.distributions[1])) for i in 1:size(rf_pred,2)]
+rf_distributions = SimpleSDMResponse.(rf_grids, sdm.distributions[1].left, sdm.distributions[1].right,
+                                         sdm.distributions[1].bottom, sdm.distributions[1].top)
+
+plotSDM(rf_distributions[24], c=:BuPu)
+plotSDM(raw.distributions[24], c=:BuPu)
+plotSDM(sdm.distributions[24], c=:BuPu)
