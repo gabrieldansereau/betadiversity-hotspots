cbc0efc5d8a85cae5796a11207b9090903f7fa91
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-03-08 17:38:18 -0400

    :wrench: compare validation metrics with 70-30 vs full training

--- src/08_random-forests.R
+++ src/08_random-forests.R
@@ -43,6 +43,11 @@ if (length(inds_withoutobs > 0)) {
   spe_test <- spe_test[, -inds_withoutobs]
 }
 
+# Create single species subset
+sp <- "sp1"
+sp_train <- as.factor(spe_train[,sp])
+sp_test <- as.factor(spe_test[,sp])
+
 #### 1. Party ####
 
 rf <- cforest(as.factor(spe$sp20) ~ .,
@@ -59,9 +64,6 @@ table(spe$sp1, sp1_pred)
 #### 2. randomForest ####
 
 set.seed(42)
-sp <- "sp1"
-sp_train <- as.factor(spe_train[,sp])
-sp_test <- as.factor(spe_test[,sp])
 system.time(
   model1 <- randomForest(sp_train ~ .,
                          data = vars_train,
@@ -128,16 +130,6 @@ hist(rf_res$OOB, breaks=20)
 boxplot(rf_res$OOB)
 
 # Test on testing subset
-rf_test <- function(sp_train, sp_test, vars, ...) {
-  set.seed(42)
-  sp_train <- as.factor(sp)
-  rf <- randomForest(sp_train ~ .,
-                     data = vars,
-                     importance = TRUE,
-                     ...)
-  return(rf)
-}
-
 system.time(rf_tests <- lapply(rf_models, function(x) predict(x, vars_test, type = "class")))
 rf_tests
 
@@ -165,9 +157,8 @@ system.time(
   rf_models <- lapply(spe_train[,1:10], function(x) rf_train(x, vars_train))
 )
 system.time(
-  ranger_models <- pblapply(spe_train[,], function(x) ranger_train(x, vars_train))
+  ranger_models <- pblapply(spe_train, function(x) ranger_train(x, vars_train))
 )
-
 
 # Parallized calls
 system.time(
@@ -177,6 +168,11 @@ system.time(
   ranger_models <- mclapply(spe_train[,], function(x) ranger_train(x, vars_train), mc.cores = 12)
 )
 
+# View results
+rf_models
+ranger_models
+
+# Combine results in dataframe
 rf_res <- data.frame(species = colnames(spe_train),
                      OOB = sapply(ranger_models, function(x) x$prediction.error),
                      error_rate_0 = sapply(ranger_models, function(x) x$confusion.matrix[3]/sum(x$confusion.matrix[c(1,3)])),
@@ -184,17 +180,59 @@ rf_res <- data.frame(species = colnames(spe_train),
 )
 rf_res
 
+# Plot results
 barplot(rf_res$OOB, names.arg = rf_res$species)
 hist(rf_res$OOB, breaks=20)
 boxplot(rf_res$OOB)
 
+# Plot presence vs absence error rate
 barplot(rf_res$error_rate_0, names.arg = rf_res$species)
-
 barplot(rf_res$error_rate_1, names.arg = rf_res$species)
+
 ## Export model
 save(ranger_models, file = "data/proc/rf_models.RData")
 
 ## Test ranger predictions
-system.time(ranger_tests <- lapply(ranger_models, function(x) predict(x, vars_test, type = "class")))
-rf_tests
+# Make predictions
+system.time(ranger_tests <- lapply(ranger_models, function(x) predict(x, vars_test)))
+ranger_tests
 
+# Extract confusion matrix and results
+ranger_confusion <- mapply(function(x,y) confusionMatrix(data = x$predictions,
+                                           reference = as.factor(y),
+                                           positive = "1"),
+                           ranger_tests, spe_test, SIMPLIFY = FALSE)
+rf_test_res <- data.frame(species = colnames(spe_test),
+                          test_OOB = sapply(ranger_confusion, function(x) 1 - x$overall["Accuracy"]),
+                          test_error_rate_0 = sapply(ranger_confusion, function(x) 1 - x$byClass["Specificity"]),
+                          test_error_rate_1 = sapply(ranger_confusion, function(x) 1 - x$byClass["Sensitivity"]))
+head(rf_test_res)
+
+# Compare training & validation errors
+(comparison <- (rf_res - rf_test_res) * 100)
+summary(comparison)
+hist(comparison$OOB) # mostly < 1
+hist(comparison$error_rate_0, breaks = 10) # mostly < 1
+hist(comparison$error_rate_1, breaks = 20) # some extremes
+cor(rf_res[,-1], rf_test_res[,-1]) # correlations of 0.999, 0.999 and 0.982
+
+## Training on full dataset
+system.time(
+  full_models <- pblapply(spe, function(x) ranger_train(x, vars))
+)
+full_models
+
+# Combine results in dataframe
+rf_full_res <- data.frame(species = colnames(spe),
+                     OOB = sapply(full_models, function(x) x$prediction.error),
+                     error_rate_0 = sapply(full_models, function(x) x$confusion.matrix[3]/sum(x$confusion.matrix[c(1,3)])),
+                     error_rate_1 = sapply(full_models, function(x) x$confusion.matrix[2]/sum(x$confusion.matrix[c(2,4)]))
+)
+
+# Compare full training with 70% training
+(full_comparison <- (rf_res - rf_full_res[-60,]) * 100)
+summary(full_comparison) # not much prediction gain
+hist(full_comparison$OOB, breaks = 20) # mostly < 1
+hist(full_comparison$error_rate_0, breaks = 20) # mostly < 1
+hist(full_comparison$error_rate_1, breaks = 20) # some extremes
+cor(rf_res[,-1], rf_full_res[-60,-1])
