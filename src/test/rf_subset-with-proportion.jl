b052a36643c5a987e144b3003bc0a353d48701cd
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-04-07 15:38:44 -0400

    :wrench: work on subsetting workflow

--- src/test/rf_subset-with-proportion.jl
+++ src/test/rf_subset-with-proportion.jl
@@ -1,38 +1,9 @@
-function get_features_and_labels(features, labels)
-end
-features = spe
-labels = var
-
-idx = collect(1:nrow(var))
-@time begin
-	idx0 = map(x -> findall(iszero, x), eachcol(features))
-	idx1 = map(x -> findall(isone, x), eachcol(features))
-
-	Random.seed!(42)
-	shuffle!.(idx0)
-	shuffle!.(idx1)
-
-	n_training0 = [convert(Int64, round(0.7*size(i, 1); digits=0)) for i in idx0]
-	n_training1 = [convert(Int64, round(0.7*size(i, 1); digits=0)) for i in idx1]
-	trn_idx0 = [i[1:n_training0] for i in idx0]
-	trn_idx1 = [i[1:n_training1] for i in idx1]
-end
-
-function random_idx_subset(idx, ratio)
-	Random.seed!(42)
-	shuffle!(idx)
-	ntraining = convert(Int64, round(ratio*size(idx, 1); digits=0))
-	trn_idx = idx[1:ntraining]
-	vld_idx = idx[ntraining+1:end]
-	return (trn = trn_idx, vld = vld_idx)
-end
-
-function separate_trn_vld(sp)
+function partition_by_class(sp)
 	idx0 = findall(iszero, sp)
 	idx1 = findall(isone, sp)
 
-	idx0_trn, idx0_vld = random_idx_subset(idx0, 0.7)
-	idx1_trn, idx1_vld = random_idx_subset(idx1, 0.7)
+	idx0_trn, idx0_vld = partition(idx0, 0.7, shuffle = true, rng = 42)
+	idx1_trn, idx1_vld = partition(idx1, 0.7, shuffle = true, rng = 42)
 
 	idx_trn = vcat(idx0_trn, idx1_trn)
 	idx_vld = vcat(idx0_vld, idx1_vld)
@@ -40,9 +11,42 @@ function separate_trn_vld(sp)
 end
 
 function get_labels(labels)
-	trn_idx, vld_idx = separate_trn_vld(labels)
+	trn_idx, vld_idx = partition_by_class(labels)
 	trn_labels, vld_labels = labels[trn_idx], labels[vld_idx]
 	return trn_labels, vld_labels
 end
 
-trn_labels, vld_labels = [get_labels(sp) for sp in eachcol(spe)]
+import MLJ.partition
+# Create empty Arrays
+trn_idx = Array{Array{Int64,1},1}()
+vld_idx = Array{Array{Int64,1},1}()
+# Collect training & validation indices
+@time for sp in eachcol(spe)
+	push!(trn_idx, separate_trn_vld(sp).trn)
+	push!(vld_idx, separate_trn_vld(sp).vld)
+end
+
+@time begin
+	# Create labels sets
+	trn_labels = [Int64.(spe[trn_idx[i], i]) for i in 1:ncol(spe)];
+	vld_labels = [Int64.(spe[vld_idx[i], i]) for i in 1:ncol(spe)];
+	# Create features sets
+	trn_features = [Array(var[trn_idx[i], :]) for i in 1:ncol(spe)];
+	vld_features = [Array(var[trn_idx[i], :]) for i in 1:ncol(spe)];
+end;
+@time begin
+	trn_labels, vld_labels = [map(i -> Int64.(spe[idx[i], i]), 1:ncol(spe)) for idx in (trn_idx, vld_idx)];
+	trn_features, vld_features = [map(i -> Array(var[i, :]), idx) for idx in (trn_idx, vld_idx)];
+end;
+@time begin
+	trn_labels = Array{Array{Int64,1},1}()
+	vld_labels = Array{Array{Int64,1},1}()
+	trn_features = Array{Array{Float64,2},1}()
+	vld_features = Array{Array{Float64,2},1}()
+	for i in 1:ncol(spe)
+		push!(trn_labels, Int64.(spe[trn_idx[i], i]))
+		push!(vld_labels, Int64.(spe[vld_idx[i], i]))
+		push!(trn_features, Array(var[trn_idx[i], :]))
+		push!(vld_features, Array(var[vld_idx[i], :]))
+	end
+end
