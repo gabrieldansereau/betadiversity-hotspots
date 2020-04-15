7edb46fccdea4516f3121748acd1f6c538833750
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-04-15 13:00:53 -0400

    :sparkles: richness random forest 1st attempt

--- src/10_richness-rf.jl
+++ src/10_richness-rf.jl
@@ -19,9 +19,53 @@ function calculate_richness(Y, inds_notobs, distributions)
   ## Create SimpleSDMLayer
   richness = SimpleSDMResponse(sums, distributions[1].left, distributions[1].right, distributions[1].bottom, distributions[1].top)
 end
-richness_NE = calculate_richness(NE.Y, NE.inds_notobs, distributions_NE)
-richness_SW = calculate_richness(SW.Y, SW.inds_notobs, distributions_SW)
+richness_raw = calculate_richness(Y, inds_notobs, distributions)
 
 # Visualize
-plot(richness_NE, c = :viridis)
-plot(richness_SW, c = :viridis)
+plotSDM(richness_raw, c = :viridis)
+
+## Extract values for model
+richness_values = Int64.(richness_raw.grid[inds_obs])
+
+## Random Forest
+using RCall
+@rput richness_values inds_obs
+begin
+  R"""
+  library(ranger)
+  spa <-  read.csv("data/proc/distributions_spa.csv", header=TRUE, sep="\t")
+  env <-  read.csv("data/proc/distributions_env.csv", header=TRUE, sep="\t")
+
+  # Remove site with NAs for landcover variables
+  (inds_withNAs <- unique(unlist(sapply(env, function(x) which(is.na(x))))))
+  if (length(inds_withNAs) > 0) {
+    richness_values <- richness_values[-inds_withNAs]
+    spa <- spa[-inds_withNAs,]
+    env <- env[-inds_withNAs,]
+  }
+
+  # Combine environmental variables
+  vars <- cbind(env, spa)
+
+  # Separate into training/testing datasets
+  set.seed(42)
+  inds_train <- sample(nrow(vars), 0.7*nrow(vars), replace = FALSE)
+
+  richness_train <- richness_values[inds_train]
+  vars_train <- vars[inds_train,]
+
+  richness_test <- richness_values[-inds_train]
+  vars_test <- vars[-inds_train,]
+
+  # Train model
+  system.time(regress_model <- ranger(richness_train ~ ., data = vars_train, importance = "impurity", seed = 42))
+  system.time(classif_model <- ranger(as.factor(richness_train) ~ ., data = vars_train, importance = "impurity", seed = 42))
+
+  regress_pred <- predict(regress_model, vars_test)$predictions
+  sum(round(regress_pred) == richness_test)/length(richness_test)
+
+  classif_pred <- predict(classif_model, vars_test)$predictions
+  sum(classif_pred == richness_test)/length(richness_test)
+
+  """
+end
