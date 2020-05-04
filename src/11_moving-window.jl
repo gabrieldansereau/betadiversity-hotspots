6880db6584fb0c5066e690b04f8102a5d6a6f7a9
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-05-04 15:08:09 -0400

    :fire: remove useless alternative calls

--- src/11_moving-window.jl
+++ src/11_moving-window.jl
@@ -21,58 +21,19 @@ Ymats = calculate_Ymatrix(distributions)
 richness = calculate_richness(Ymats.Y, Ymats.inds_notobs, distributions)
 lcbd = calculate_lcbd(Ymats.Yobs, Ymats.Ytransf, Ymats.inds_obs, distributions)
 
-plotSDM(richness, c = :viridis)
+plot(richness, c = :viridis)
 
-## Alternative analysis functions
-
-# Previous workflow
-@time begin
-  Ymats = calculate_Ymatrix(distributions);
-  @time richness = calculate_richness(Ymats.Y, Ymats.inds_notobs, distributions);
-  @time lcbd = calculate_lcbd(Ymats.Yobs, Ymats.Ytransf, Ymats.inds_obs, distributions);
-end;
-
-# New workflow
-@time begin
-  Y = calculate_Y(distributions, transform = false);
-  @time richness = calculate_richness(Y, distributions);
-  @time lcbd = calculate_lcbd(Y, distributions; relative = true);
-end;
-
-## Moving windows
-
-function get_windows(mat, pos, wsize; step = 1)
-  # Get all windows
-  newmats = []
-  for j in 1:step:size(mat,2)-(wsize[2]-1), i in 1:step:size(mat,1)-(wsize[1]-1)
-    subrows, subcols = i:i+(wsize[1]-1), j:j+(wsize[2]-1)
-    submat = mat[subrows, subcols]
-    subpos = pos[subrows, subcols]
-    subinds = indexin(vec(subpos), vec(pos))
-    newmat = fill(NaN, size(mat))
-    newmat[subinds] = submat
-    push!(newmats, newmat)
-  end
-  return newmats
-end
+## Prepare subareas
 
 # Define subareas
 coords_NE = (left = -80.0, right = -60.0, bottom = 40.0, top = 50.0)
 coords_SW = (left = -120.0, right = -100.0, bottom = 30.0, top = 40.0)
 distributions_NE = [d[coords_NE] for d in distributions]
-# Smaller test area
-distributions_full = copy(distributions)
-distributions = copy(distributions_NE)
-coords_mini = (left = -80.0, right = -70.0, bottom = 40.0, top = 45.0)
-distributions_mini = [d[coords_mini] for d in distributions]
-wsize = size(distributions_mini[1])
-grid_pos = CartesianIndices(distributions[1].grid)
 # Define window size
 wsize = size(distributions_NE[1])
-# Extract grid positions
-grid_pos = CartesianIndices(distributions[1].grid)
 
-## New version
+## Moving windows
+
 function get_windows_indices(index_mat, wsize; step = 1)
   # Get all windows
   winds = []
@@ -108,52 +69,10 @@ end
   LCBDmean = map(x -> mean(filter(!isnan, x)), eachrow(LCBDmat))
   # Arrange mean values as layer
   LCBDwindow = similar(distributions[1])
-  LCBDwindow.grid = reshape(LCBDmean, size(LCBDwindow.grid))
-  LCBDwindow
-end;
-LCBDwindow_new = copy(LCBDwindow)
-plotSDM(LCBDwindow_new, c = :viridis)
-
-## Previous option
-@time begin
-  # Get distribution windows (per species)
-  dwindows = map(d -> get_windows(d.grid, grid_pos, wsize; step = 10), distributions);
-
-  # Arrange windows in a 2D array
-  dmats = [d[i] for i in eachindex(dwindows[1]), d in dwindows]
-  size(dmats)
-  # Arrange as layers in 2D array
-  dlayers = [SimpleSDMResponse(d, distributions[1].left, distributions[1].right,
-                                distributions[1].bottom, distributions[1].top) for d in dmats]
-  size(dlayers)
-
-  # Get Ymatrices
-  Ybatch = [calculate_Y(distributions) for distributions in eachrow(dlayers)]
-  # Remove ones without observation
-  filter!(x -> !all(isnan, x), Ybatch)
-  # Get LCBD values
-  LCBDbatch = [calculate_lcbd(Y, distributions; relative = true) for Y in Ybatch]
-
-  # Arrange LCBD values as matrix
-  LCBDmat = reduce(hcat, [vec(LCBD[2].grid) for LCBD in LCBDbatch])
-  # Get mean LCBD value per site
-  LCBDmean = map(x -> mean(filter(!isnan, x)), eachrow(LCBDmat))
-  # Arrange mean values as layer
-  LCBDwindow = similar(distributions[1])
+  LCBDwindow.grid .= NaN
   LCBDwindow.grid = reshape(LCBDmean, size(LCBDwindow.grid))
 end;
-LCBDwindow_prev = copy(LCBDwindow)
-plotSDM(LCBDwindow_prev, c = :viridis)
-
-# Time comparison
-# prev: 1.7 sec, 5.50M alloc, 1.662 GB
-# new: 0.5sec, 3.07M alloc, 334.514 MB
-# Compare number of elements in options
-nprev = (62*7000) + (62*28*7000) + (28*62*7000) + (28*7000) + (7000)
-nnew = (62*7000) + (1*62*7000) + (28*62*1800) + (28*1800) + (28*7000) + (7000)
-nnew/nprev # 17% of previous nvals
-# Compare results
-filter(!isnan, LCBDwindow_prev.grid) == filter(!isnan, LCBDwindow_new.grid)
+plot(LCBDwindow, c = :viridis)
 
 ## Visualize result
 function plot_lcbd_windows(richness, lcbd; title = "", kw...)
