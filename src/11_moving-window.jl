52b4250008c926b5c77c6e464b615e7f97b32e20
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-05-04 09:27:09 -0400

    :fire: remove dummy example

--- src/11_moving-window.jl
+++ src/11_moving-window.jl
@@ -23,53 +23,6 @@ lcbd = calculate_lcbd(Ymats.Yobs, Ymats.Ytransf, Ymats.inds_obs, distributions)
 
 plotSDM(richness, c = :viridis)
 
-## Dummy example
-
-# Dummy set
-mat = Array(reshape(1.:12., (3,4)))
-pos = Array(reshape(101.:112., (3,4)))
-# Window size
-wsize = (2,2)
-# Number of iterations
-rowit = size(mat,1) - wsize[1] + 1
-colit = size(mat,2) - wsize[2] + 1
-totit = rowit * colit
-# Cartesian indices
-cartinds = findall(!isnan, mat)
-mat[cartinds[1]]
-function get_windows(mat, pos, wsize; step = 1)
-  # Get all windows
-  newmats = []
-  for j in 1:step:size(mat,2)-(wsize[2]-1), i in 1:step:size(mat,1)-(wsize[1]-1)
-    subrows, subcols = i:i+(wsize[1]-1), j:j+(wsize[2]-1)
-    submat = mat[subrows, subcols]
-    subpos = pos[subrows, subcols]
-    subinds = indexin(vec(subpos), vec(pos))
-    newmat = fill(NaN, size(mat))
-    newmat[subinds] = submat
-    push!(newmats, newmat)
-  end
-  return newmats
-end
-newmats = get_windows(mat, pos, wsize)
-
-# Reduce to minimum value
-@time reduce(min, map(vec, newmats)) |> x-> reshape(x, size(mat))
-@time mapreduce(vec, min, newmats) |> x-> reshape(x, size(mat))
-# Combine values
-@time mapreduce(vec, hcat, newmats)
-@time reduce(hcat, map(vec, newmats))
-
-# Custom mean function
-function mean_nonan(x, y)
-  mat_combined = mapreduce(vec, hcat, [x, y])
-  mat_mean = map(x -> mean(filter(!isnan, x)), eachrow(mat_combined))
-  mat_mean = reshape(mat_mean, size(x))
-end
-# Reduce by mean
-mean_nonan(newmats[1], newmats[2])
-reduce(mean_nonan, newmats)
-
 ## Alternative analysis functions
 
 # Previous workflow
@@ -88,6 +41,21 @@ end;
 
 ## Moving windows
 
+function get_windows(mat, pos, wsize; step = 1)
+  # Get all windows
+  newmats = []
+  for j in 1:step:size(mat,2)-(wsize[2]-1), i in 1:step:size(mat,1)-(wsize[1]-1)
+    subrows, subcols = i:i+(wsize[1]-1), j:j+(wsize[2]-1)
+    submat = mat[subrows, subcols]
+    subpos = pos[subrows, subcols]
+    subinds = indexin(vec(subpos), vec(pos))
+    newmat = fill(NaN, size(mat))
+    newmat[subinds] = submat
+    push!(newmats, newmat)
+  end
+  return newmats
+end
+
 # Define subareas
 coords_NE = (left = -80.0, right = -60.0, bottom = 40.0, top = 50.0)
 coords_SW = (left = -120.0, right = -100.0, bottom = 30.0, top = 40.0)
@@ -98,13 +66,14 @@ wsize = size(distributions_NE[1])
 grid_pos = CartesianIndices(distributions[1].grid)
 
 # Get distribution windows (per species)
-@time dwindows = @showprogress map((d, pos) -> get_windows(d.grid, pos, wsize; step = 33), distributions, grid_pos);
+@time dwindows = @showprogress map(d -> get_windows(d.grid, grid_pos, wsize; step = 36), distributions);
 
 # Arrange windows in a 2D array
 dmats = [d[i] for i in eachindex(dwindows[1]), d in dwindows]
 size(dmats)
 # Arrange as layers in 2D array
-dlayers = [SimpleSDMResponse(d, distributions[1].left, distributions[1].right, distributions[1].bottom, distributions[1].top) for d in dmats]
+dlayers = [SimpleSDMResponse(d, distributions[1].left, distributions[1].right,
+                              distributions[1].bottom, distributions[1].top) for d in dmats]
 size(dlayers)
 
 # Get Ymatrices
