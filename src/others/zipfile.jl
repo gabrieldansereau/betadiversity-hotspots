f5c6441a51be16959012b0c5883a73098347479f
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-05-07 20:23:49 -0400

    :sparkles: function to unzip JLD2 files if missing

--- src/others/zipfile.jl
+++ src/others/zipfile.jl
@@ -29,6 +29,36 @@ zfdist = copy(distributions)
 # Compare results
 map(x -> filter(!isnan, x.grid), zfdist) == map(x -> filter(!isnan, x.grid), rfdist)
 
+## Create custom functions
+
+function _unzip_jld2(zip_path, jld_path)
+    zf = ZipFile.Reader(zip_path)
+    open(jld_path, "w") do io
+        write(io, read(zf.files[1]))
+    end
+end
+function verify_jld2_data(path)
+    # List zip files
+    files = readdir(path, join = true)
+    zipfiles = filter(x -> occursin(".zip", x), files)
+    # Check if corresponding .jld2 exist
+    jldfiles = replace.(zipfiles, ".zip" => ".jld2")
+    missing_files = map(!isfile, jldfiles)
+    # Extract missing files from archive & write to disk
+    if any(missing_files)
+        for i in findall(missing_files)
+            @info "Reading $(jldfiles[i]) from archive ($(i)/$(sum(missing_files)))"
+            _unzip_jld2(zipfiles[i], jldfiles[i])
+        end
+    else
+        @info "All JLD2 files exist"
+    end
+end
+@time verify_jld2_data(joinpath("data", "jld2"))
+
+
+#############
+
 ## Zipfile documentation
 using ZipFile
 # Zip files
