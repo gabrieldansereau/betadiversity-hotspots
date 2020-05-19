06fd2239f66f32ab85c6b4e62ea314d718b5d8a8
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-05-19 16:14:19 -0400

    :wrench: minor updates to RF predictions script

--- src/03_predictions_random-forests.jl
+++ src/03_predictions_random-forests.jl
@@ -4,18 +4,10 @@ using Distributed
 @time include("required.jl")
 
 ## Conditional arguments
-# save_figures = true
-
-## Load data
-#=
-spe = CSV.read(joinpath("data", "proc", "distributions_spe_full.csv"), header=true, delim="\t")
-spa = CSV.read(joinpath("data", "proc", "distributions_spa_full.csv"), header=true, delim="\t")
-env = CSV.read(joinpath("data", "proc", "distributions_env_full.csv"), header=true, delim="\t")
-=#
+# save_data = true
 
 ## Perform RandomForests
 using RCall
-# @rput spe spa env
 begin
     R"""
     library(ranger)
@@ -56,41 +48,36 @@ end
 @rget predictions predictions_full inds_na
 
 ## Create Y matrices
+
 # Get matrix Y
 Y = replace(predictions_full, missing => NaN)
 # Set values to NaN if no species present
 inds_zeros = findall(map(x -> all(iszero.(x)), eachrow(Y)))
 Y[inds_zeros,:] .= NaN
-# Get Yobs
-inds_obs = findall(map(x -> !any(isnan.(x)), eachrow(Y)))
-inds_notobs = findall(map(x -> any(isnan.(x)), eachrow(Y)))
-Yobs = Y[inds_obs,:]
-# Apply Hellinger transformation
-@rput Yobs
-begin
-    R"""
-        library(vegan)
-        Ytransf <- decostand(Yobs, "hel")
-    """
-end
-@rget Ytransf
 
 ## Create distributions
 
 # Load raw distributions (for grid size)
 @load joinpath("data", "jld2", "raw-distributions.jld2") distributions
+raw_distributions = distributions
+# Get layer dimensions & limits
+dims = size(raw_distributions[1].grid)
+lims = (left = raw_distributions[1].left, right = raw_distributions[1].right,
+        bottom = raw_distributions[1].bottom, top = raw_distributions[1].top)
 
 # Create RF distribution layers
 Ydistrib = replace(Y, 0.0 => NaN)
-rf_grids = [reshape(Ydistrib[:,i], size(distributions[1].grid)) for i in 1:size(Ydistrib, 2)]
-# map(x -> reshape(Ydistrib[:,x], size(distributions[1]), 1:size(Ydistrib, 2)))
-distributions = SimpleSDMResponse.(rf_grids, distributions[1].left, distributions[1].right,
-                                         distributions[1].bottom, distributions[1].top)
+Ygrids = [Ydistrib[:, col] for col in 1:size(Ydistrib,2)]
+Ygrids = reshape.(Ygrids, dims...)
+distributions = SimpleSDMResponse.(Ygrids, lims...)
 
 ## Export results
 # save_data = true
 if @isdefined save_data && save_data == true
     @save joinpath("data", "jld2", "rf-distributions.jld2") distributions
+    _zip_jld2(joinpath("data", "jld2", "rf-distributions.zip"),
+              joinpath("data", "jld2", "rf-distributions.jld2"))
+    touch(joinpath("data", "jld2", "rf-distributions.jld2"))
 end
 
 ## Get richness & LCBD
