2bdd534a09000c878871c1416449f324f91991a3
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-05-28 12:46:16 -0400

    :wrench: update RF script for consistency with BART & tidyverse syntax

--- src/02_training_random-forests.R
+++ src/02_training_random-forests.R
@@ -1,41 +1,34 @@
+## 0. Load packages ####
+library(conflicted)
+library(tidyverse)
 library(ranger)
 library(caret)
-library(parallel)
 library(pbapply)
 
-#### 0. Load data ####
+# Resolve conflicts
+conflict_prefer("filter", "dplyr")
+conflict_prefer("intersect", "dplyr")
+
+# Conditional evaluations
+subset_qc <- TRUE # subset to QC data (optional)
+# create_models <- TRUE # train models
+# save_models <- TRUE # save & overwrite models
+
+## 1. Load data ####
+
+message("Loading & preparing data")
+
 # Load data
-spe <- read.csv("data/proc/distributions_spe_full.csv", header=TRUE, sep="\t")
-spa <- read.csv("data/proc/distributions_spa_full.csv", header=TRUE, sep="\t")
-env <- read.csv("data/proc/distributions_env_full.csv", header=TRUE, sep="\t")
+spa_full <- read_tsv("data/proc/distributions_spa_full.csv")
+env_full <- read_tsv("data/proc/distributions_env_full.csv")
+spe      <- read_tsv("data/proc/distributions_spe_full.csv") 
 
-# Select observed sites only
-inds_obs <- spe$site
+# Load QC data (optional)
+spa_qc <- read_tsv("data/proc/distributions_spa_qc.csv")
 
-# Subset to QC data (optional)
-# spa_qc <- read.csv("data/proc/distributions_spa_qc.csv", header=TRUE, sep="\t")
-# inds_obs <- intersect(spa_qc$site, inds_obs)
-# spe <- spe[spe$site %in% inds_obs,]
-
-# Filter datasets to observed sites only
-spa <- spa[inds_obs,]
-env <- env[inds_obs,]
-
-# Remove site with NAs for landcover variables
-(inds_withNAs <- unique(unlist(sapply(env, function(x) which(is.na(x))))))
-if (length(inds_withNAs) > 0) {
-    spe <- spe[-inds_withNAs,]
-    spa <- spa[-inds_withNAs,]
-    env <- env[-inds_withNAs,]
-}
-
-# Combine environmental variables
-vars <- merge(env, spa, by = "site")
-# Remove site column
-spe <- subset(spe, select = -site)
-env <- subset(env, select = -site)
-spa <- subset(spa, select = -site)
-vars <- subset(vars, select = -site)
+# Prepare data
+# subset_qc <- TRUE # subset to QC data (optional)
+source("src/02_training_data-preparation.R")
 
 # Separate into training/testing datasets
 set.seed(42)
@@ -59,23 +52,34 @@ if (length(inds_withoutobs > 0)) {
 }
 
 # Create single species subset
-sp <- "sp1"
-sp_train <- as.factor(spe_train[,sp])
-sp_test <- as.factor(spe_test[,sp])
+sp <- "sp17"
+sp_train <- spe_train[sp]
+sp_test <- as.factor(spe_test[[sp]])
 
 #### Ranger ####
 
 # Single species time test
-system.time(ranger_model <- ranger(sp_train ~ ., data = vars_train, importance = "impurity", seed = 42))
+system.time(
+    ranger_model <- ranger(
+        sp_train ~ ., 
+        data = vars_train,
+        classification = TRUE, 
+        importance = "impurity", 
+        seed = 42
+    )
+)
 
-## Wrap as function
+# Wrap as function
 ranger_train <- function(sp, vars, ...) {
     sp_train <- as.factor(sp)
-    rf <- ranger(sp_train ~ .,
-                 data = vars,
-                 importance = "impurity",
-                 seed = 42,
-                 ...)
+    rf <- ranger(
+        sp_train ~ .,
+        data = vars,
+        classification = TRUE,
+        importance = "impurity",
+        seed = 42,
+        ...
+    )
     return(rf)
 }
 
@@ -84,19 +88,15 @@ system.time(
     ranger_models <- pblapply(spe_train, function(x) ranger_train(x, vars_train))
 )
 
-# Alternative parallized call
-# system.time(
-#     ranger_models <- mclapply(spe_train[,], function(x) ranger_train(x, vars_train), mc.cores = 12)
-# )
-
 # View results
 ranger_models
 
 # Combine results in dataframe
-rf_res <- data.frame(species = colnames(spe_train),
-                     OOB = sapply(ranger_models, function(x) x$prediction.error),
-                     error_rate_0 = sapply(ranger_models, function(x) x$confusion.matrix[3]/sum(x$confusion.matrix[c(1,3)])),
-                     error_rate_1 = sapply(ranger_models, function(x) x$confusion.matrix[2]/sum(x$confusion.matrix[c(2,4)]))
+rf_res <- tibble(
+    species = colnames(spe_train),
+    OOB = map_dbl(ranger_models, "prediction.error"),
+    error_rate_0 = map_dbl(ranger_models, ~ .x$confusion.matrix[3]/sum(.x$confusion.matrix[c(1,3)])),
+    error_rate_1 = map_dbl(ranger_models, ~ .x$confusion.matrix[2]/sum(.x$confusion.matrix[c(2,4)]))
 )
 rf_res
 
@@ -114,22 +114,28 @@ save(ranger_models, file = "data/proc/rf_models.RData")
 
 ## Test ranger predictions
 # Make predictions
-system.time(ranger_tests <- lapply(ranger_models, function(x) predict(x, vars_test)))
+system.time(ranger_tests <- map(ranger_models, ~ predict(.x, vars_test)))
 ranger_tests
 
 # Extract confusion matrix and results
-ranger_confusion <- mapply(function(x,y) confusionMatrix(data = x$predictions,
-                                           reference = as.factor(y),
-                                           positive = "1"),
-                           ranger_tests, spe_test, SIMPLIFY = FALSE)
-rf_test_res <- data.frame(species = colnames(spe_test),
-                          test_OOB = sapply(ranger_confusion, function(x) 1 - x$overall["Accuracy"]),
-                          test_error_rate_0 = sapply(ranger_confusion, function(x) 1 - x$byClass["Specificity"]),
-                          test_error_rate_1 = sapply(ranger_confusion, function(x) 1 - x$byClass["Sensitivity"]))
-head(rf_test_res)
+ranger_confusion <- map2(
+    ranger_tests, spe_test,
+    ~ confusionMatrix(
+        data = .x$predictions,
+        reference = as.factor(.y),
+        positive = "1"
+    )
+)
+rf_test_res <- tibble(
+    species = colnames(spe_test),
+    test_OOB = map_dbl(ranger_confusion, ~ 1 - .x$overall["Accuracy"]),
+    test_error_rate_0 = map_dbl(ranger_confusion, ~ 1 - .x$byClass["Specificity"]),
+    test_error_rate_1 = map_dbl(ranger_confusion, ~ 1 - .x$byClass["Sensitivity"])
+)
+rf_test_res
 
 # Compare training & validation errors
-(comparison <- (rf_res - rf_test_res) * 100)
+(comparison <- (select(rf_res, -species) - select(rf_test_res, -species)) * 100) %>% as_tibble()
 summary(comparison)
 hist(comparison$OOB) # mostly < 1
 hist(comparison$error_rate_0, breaks = 10) # mostly < 1
