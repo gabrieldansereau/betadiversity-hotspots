9992d45cc40888e529405a84da9a12663ccdd5ae
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-06-02 19:06:05 -0400

    :sparkles: direct LCBD RF model

--- src/others/explore_direct-models_rf.R
+++ src/others/explore_direct-models_rf.R
@@ -13,46 +13,61 @@ subset_qc <- TRUE # subset to QC data (optional)
 # Load data
 source(here("src", "02a_training_data-preparation.R"))
 
-# Get richness values
+## Get richness values
 richness_values <- rowSums(spe)
+
+## Get LCBD values
+# Apply Hellinger transformation
+spe_transf <- vegan::decostand(spe_full[inds_obs,-1], "hel")
+# S -> squared deviations from column mean
+S <- map_df(spe_transf, ~ (.x - mean(.x))^2)
+# SStotal -> total sum of squares
+SStotal <- sum(S)
+# SSi -> sum of squares for site i
+SSi <- rowSums(S)
+# LCBD -> local contribution to beta diversity (site i, relative)
+lcbd_values <- SSi/SStotal
+# Set values relative to maximum
+lcbd_values <- lcbd_values/max(lcbd_values)
+# Remove sites with NaN for landcover variables
+if (length(inds_withNAs) > 0) {
+    lcbd_values <- lcbd_values[-inds_withNAs]
+}
+
+# Group richness & lcbd
+values_df <- tibble(
+    richness = richness_values,
+    lcbd = lcbd_values
+)
 
 # Separate into training/testing datasets
 set.seed(42)
 inds_train <- sample(nrow(vars), 0.7*nrow(vars), replace = FALSE)
 
-richness_train <- richness_values[inds_train]
+values_train <- values_df[inds_train,]
 vars_train <- vars[inds_train,]
 
-richness_test <- richness_values[-inds_train]
+values_test <- values_df[-inds_train,]
 vars_test <- vars[-inds_train,]
 
 
 ## 2. Train model ####
-# Regression model
+# Regression models
 system.time(
-    regress_model <- ranger(
-        richness_train ~ ., 
-        data = vars_train, 
-        importance = "impurity", 
-        seed = 42
-    )
-)
-# Classification model
-system.time(
-    classif_model <- ranger(
-        richness_train ~ ., 
-        data = vars_train, 
-        classification = TRUE, 
-        importance = "impurity", 
-        seed = 42
+    models <- map(
+        values_train,
+        ~ ranger(
+            .x ~ ., 
+            data = vars_train, 
+            importance = "impurity", 
+            seed = 42
+        )
     )
 )
 
 # Check accuracy
-regress_pred <- predict(regress_model, vars_test)$predictions
+pred_test <- map_df(models, ~ predict(.x, vars_test)$predictions)
 sum(round(regress_pred) == richness_test)/length(richness_test)
-classif_pred <- predict(classif_model, vars_test)$predictions
-sum(classif_pred == richness_test)/length(richness_test)
 
 
 ## 3. Full-scale predictions ####
@@ -63,11 +78,14 @@ inds_na <- map(env_full, ~ which(is.na(.x)))
 vars_nona <- vars_full[-inds_na,]
 
 # Make predictions
-predictions_nona <- predict(classif_model, vars_nona)$predictions
+predictions_nona <- map_df(models, ~ predict(.x, vars_nona)$predictions)
 
 # Add sites with NAs
-predictions <- matrix(NA, nrow = nrow(vars_full), ncol = 1)
-colnames(predictions) <- colnames(predictions_nona)
+predictions <- tibble(
+    richness = as.numeric(NA),
+    lcbd = as.numeric(NA),
+    .rows = nrow(vars_full)
+)
 predictions[-inds_na,] <- predictions_nona
 
 # Put final result in list
