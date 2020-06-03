98696edae24cd2c7e66df4182edbb7dcc2bf0469
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-06-03 12:02:36 -0400

    :wrench: separate env & spa in different files

--- src/others/data_demo-export.R
+++ src/others/data_demo-export.R
@@ -6,13 +6,22 @@ subset_qc <- TRUE
 source(here("src", "02a_training_data-preparation.R"))
 
 # Check data
+spa
+env
 spe
-vars
+spa_full
+env_full
 spe_full
-vars_full
+
+# Remove site from full range sets
+env_full <- select(env_full, -site)
+spe_full <- select(spe_full, -site)
 
 # Export data
-write_tsv(spe,  here("..", "embarcadero-demo", "data", "spe.csv"))
-write_tsv(vars, here("..", "embarcadero-demo", "data", "env.csv"))
-write_tsv(spe_full,  here("..", "embarcadero-demo", "data", "spe_full.csv"))
-write_tsv(vars_full, here("..", "embarcadero-demo", "data", "env_full.csv"))
+write_tsv(spe, here("..", "embarcadero-demo", "data", "spe.csv"))
+write_tsv(spa, here("..", "embarcadero-demo", "data", "spa.csv"))
+write_tsv(env, here("..", "embarcadero-demo", "data", "env.csv"))
+write_tsv(spe_full, here("..", "embarcadero-demo", "data", "spe_full.csv"))
+write_tsv(spa_full, here("..", "embarcadero-demo", "data", "spa_full.csv"))
+write_tsv(env_full, here("..", "embarcadero-demo", "data", "env_full.csv"))
+
