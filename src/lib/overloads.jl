9c889eee2bd226117b86c529020ec9b9d536492e
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-06-12 16:34:10 -0400

    :wrench: no need for GBIF overloads anymore

--- src/lib/overloads.jl
+++ src/lib/overloads.jl
@@ -7,20 +7,6 @@ import SimpleSDMLayers: latitudes
 
 ## Extract layer values
 
-# Extract layer value from single GBIFRecord
-function Base.getindex(p::SimpleSDMLayer, r::GBIFRecord)
-    return p[r.longitude, r.latitude]
-end
-
-# Extract layer value from multiple GBIFRecords
-function Base.getindex(p::SimpleSDMLayer, r::GBIFRecords)
-    observations = eltype(p.grid)[]
-    for record in r
-        push!(observations, p[record])
-    end
-    return observations
-end
-
 # Extract layer value from DataFrame (with longitude & latitude columns)
 function Base.getindex(p::SimpleSDMLayer, d::DataFrame)
     observations = eltype(p.grid)[]
@@ -32,29 +18,11 @@ end
 
 ## Extract all latitudes & longitudes
 
-# Extract longitudes from GBIFRecords
-function longitudes(r::GBIFRecords)
-    l = Float64[]
-    for record in r
-        push!(l, record.longitude)
-    end
-    return l
-end
-
 # Extract longitudes from DataFrame
 function longitudes(d::DataFrame)
     l = Float64[]
     for lon in d.longitude
         push!(l, lon)
-    end
-    return l
-end
-
-# Extract latitudes from GBIFRecords
-function latitudes(r::GBIFRecords)
-    l = Float64[]
-    for record in r
-        push!(l, record.latitude)
     end
     return l
 end
