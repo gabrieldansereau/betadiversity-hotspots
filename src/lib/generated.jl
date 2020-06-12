d8e935ace65ddadfe968dbe7c4708e3cb7b86455
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-06-12 12:45:03 -0400

    :wrench: some comments

--- src/lib/generated.jl
+++ src/lib/generated.jl
@@ -34,15 +34,17 @@ for op in ops_math
     end)
 end
 
+# Test layers
 layer1 = prob_distrib[1]
 layer2 = prob_distrib[2]
 
+# Test addition
 +(layer1.grid, layer2.grid)
 +(layer1, layer2)
 +(layer1, layer2).grid
-
 layer1 + worldclim(1)
 
+# Test on array of layers
 prob_distrib[1] + prob_distrib[2] + prob_distrib[3]
 tmp = reduce(+, prob_distrib)
 plotSDM2(tmp, c = :viridis)
@@ -54,20 +56,22 @@ plotSDM2(reduce(+, upper_distrib), c = :viridis)
 
 uncertainty = upper_distrib .- lower_distrib
 plotSDM2(reduce(+, uncertainty))
-plotSDM2(reduce(mean, uncertainty))
+plotSDM2(reduce(mean, uncertainty)) # not working
 
 grids = map(x -> x.grid, uncertainty)
-heatmap(mean(grids))
+heatmap(mean(grids)) # yeah ok, need to implement elementwise mean
 
-layer1.grid .* layer2.grid
-layer1.grid * layer2.grid
+# Test multiplication
+layer1.grid * layer2.grid # matrix product, not working
+layer1.grid .* layer2.grid # elementwise product
 .*(layer1.grid, layer2.grid)
 broadcast(*, layer1.grid, layer2.grid)
 @which broadcast(*, layer1.grid, layer2.grid)
-plotSDM2(reduce(*, prob_distrib))
+plotSDM2(reduce(*, prob_distrib)) # elementwise layer product
 
-layer1.grid ./ layer2.grid
-layer1.grid / layer2.grid
+# Test division
+layer1.grid / layer2.grid # no idea what this is
+layer1.grid ./ layer2.grid # elementwise division
 ./(layer1.grid, layer2.grid)
 
 
