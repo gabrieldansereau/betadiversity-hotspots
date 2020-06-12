0e99c8f665b794b924955806fe327731d4bd0638
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-06-12 16:20:42 -0400

    :sparkles: clean layer functions script

--- src/lib/generated.jl
+++ src/lib/generated.jl
@@ -1,5 +1,7 @@
-import Base: maximum, minimum, sum, +, -, *, /
+import Base: maximum, minimum, sum, +, -, *, /, min, max
 import Statistics: mean, median, std
+
+## Overloads for single layers
 
 ops = Symbol.((
     "sum", "maximum", "minimum",
@@ -21,8 +23,10 @@ for op in ops, ty in (:SimpleSDMResponse, :SimpleSDMPredictor)
     end)
 end
 
-ops_math = Symbol.(("+", "-", "*", "/"))
+## Overloads for multiple layers
 
+# Math operations
+ops_math = Symbol.(("+", "-", "*", "/"))
 for op in ops_math
     eval(quote
         function $op(layer1::SimpleSDMLayer, layer2::SimpleSDMLayer)
@@ -34,79 +38,7 @@ for op in ops_math
     end)
 end
 
-# Test layers
-layers = copy(prob_distrib)
-layer1 = layers[1]
-layer2 = layers[2]
-
-# Test addition
-+(layer1.grid, layer2.grid)
-+(layer1, layer2)
-+(layer1, layer2).grid
-layer1 + worldclim(1)
-
-# Test on array of layers
-layers[1] + layers[2] + layers[3]
-tmp = reduce(+, layers)
-plotSDM2(tmp, c = :viridis)
-histogram(tmp)
-sort(unique(tmp.grid))
-
-plotSDM2(reduce(+, lower_distrib), c = :viridis)
-plotSDM2(reduce(+, upper_distrib), c = :viridis)
-
-uncertainty = upper_distrib .- lower_distrib
-plotSDM2(reduce(+, uncertainty))
-plotSDM2(reduce(mean, uncertainty)) # not working
-plotSDM2(reduce(mean, layers)) # not working either
-
-grids = map(x -> x.grid, layers)
-grid1 = grids[1]
-grid2 = grids[2]
-heatmap(mean(grids), c = :viridis) # yeah ok, need to implement elementwise mean
-
-# Test multiplication
-layer1.grid * layer2.grid # matrix product, not working
-layer1.grid .* layer2.grid # elementwise product
-.*(layer1.grid, layer2.grid)
-broadcast(*, layer1.grid, layer2.grid)
-@which broadcast(*, layer1.grid, layer2.grid)
-plotSDM2(reduce(*, prob_distrib)) # elementwise layer product
-
-# Test division
-layer1.grid / layer2.grid # no idea what this is
-layer1.grid ./ layer2.grid # elementwise division
-./(layer1.grid, layer2.grid)
-
-# Extend broadcast
-# broadcast(f::Tf, As...) where Tf in Base.Broadcast
-import Base.Broadcast: broadcast
-function broadcast(f, layer::SimpleSDMLayer, As...)
-    newlayer = copy(layer)
-    newlayer.grid = broadcast(f, layer.grid, As...)
-    return newlayer
-end  
-
-# Test extended broadcast
-broadcast(+, layer1, 1)
-layer1 + 1
-layer1.grid .+ 1
-@which broadcast(+, layer1.grid, 1)
-broadcast(+, layer1, 1).grid
-@which .+(layer1, 1)
-broadcast(+, layer1, layer2)
-layer1 + layer2
-
-# Test min
-reduce(min, grids) # not working
-min.(grid1, grid2) # works
-@time min.(grids...) # sooo long
-@time reduce((x,y) -> min.(x,y), grids) # sooo fast
-@time reduce((x,y) -> broadcast(min, x, y), grids) # sooo fast
-
-@time min.(layer1.grid, layer2.grid)
-
-import Base: min, max
+# Min/max
 for op in (:min, :max)
     eval(quote
         function $op(layer1::SimpleSDMLayer, layer2::SimpleSDMLayer)
@@ -120,20 +52,7 @@ for op in (:min, :max)
     end)
 end
 
-@time min(layer1, layer2)
-@time reduce(min, layers);
-@time reduce(minimum, layers);
-
-# Test mean
-reduce(mean, grids) # interesting, mean doesn't work with reduce
-mean(grid1, grid2) # not working
-mean.(grid1, grid2) # not working either
-mean([grid1, grid2]) # works
-mean(grids) # nice!
-@which mean(grids) # huh, what method is that??
-cat(grids..., dims = 3) |> x -> mean(x, dims = 3)
-
-import Base: mean, std
+# Mean/std
 for op in (:mean, :std)
     eval(quote
         function $op(layers::Array{T}) where {T <: SimpleSDMLayer}
@@ -143,7 +62,3 @@ for op in (:mean, :std)
         end
     end)
 end
-
-mean(layers)
-plot(mean(layers))
-##
