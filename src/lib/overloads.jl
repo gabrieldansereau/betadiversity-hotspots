3bd8503559e101bed01f3d810be16c6ce686f62b
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-06-12 16:38:55 -0400

    :wrench: update previous overloads

--- src/lib/overloads.jl
+++ src/lib/overloads.jl
@@ -5,18 +5,16 @@ import Statistics: mean, median, std
 import SimpleSDMLayers: longitudes
 import SimpleSDMLayers: latitudes
 
-## Extract layer values
+## DataFrame overloads
 
-# Extract layer value from DataFrame (with longitude & latitude columns)
-function Base.getindex(p::SimpleSDMLayer, d::DataFrame)
-    observations = eltype(p.grid)[]
+# Extract layer values from DataFrame (with longitude & latitude columns)
+function Base.getindex(layer::SimpleSDMLayer, d::DataFrame)
+    observations = eltype(layer.grid)[]
     for i in 1:nrow(d)
-        push!(observations, p[d.longitude[i], d.latitude[i]])
+        push!(observations, layer[d.longitude[i], d.latitude[i]])
     end
     return observations
 end
-
-## Extract all latitudes & longitudes
 
 # Extract longitudes from DataFrame
 function longitudes(d::DataFrame)
@@ -36,13 +34,13 @@ function latitudes(d::DataFrame)
     return l
 end
 
-## Other layer manipulations
-
-# Clip layer to DataFrame/GBIFRecords occurrences extent
-function clip(p::SimpleSDMLayer, r::Union{GBIFRecords,DataFrame})
+# Clip layer to extent of DataFrame occurrences
+function clip(layer::SimpleSDMLayer, r::DataFrame)
     lats = latitudes(r)
     lons = longitudes(r)
-    return p[(minimum(lons)-1.0, maximum(lons)+1.0), (minimum(lats)-1.0, maximum(lats)+1.0)]
+    clip_coords = (left = minimum(lons)-1.0, right = maximum(lons)+1.0,
+                   bottom = minimum(lats)-1.0, top = maximum(lats)+1.0)
+    return layer[clip_coords]
 end
 
 ## Overloads for single layers
