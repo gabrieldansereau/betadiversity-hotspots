9a66625eb1a04df797b5c5a5a785977cfd410e90
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-06-26 14:38:52 -0400

    :sparkles: raw richness without title

--- docs/article/article-plan.jl
+++ docs/article/article-plan.jl
@@ -3,18 +3,22 @@ Pkg.activate(".")
 using Distributed
 @time include(joinpath("..", "..", "src", "required.jl"))
 
-outcome = "bart"
-
 # Run analyses
+outcome = "raw"
+@time include(joinpath("..", "..", "src", "04_analysis.jl"))
+richness_rawplot = richness_plot
+
+outcome = "bart"
 @time include(joinpath("..", "..", "src", "04_analysis.jl"))
 
 # Remove figures titles
+plot!(richness_rawplot, title = "")
 plot!(richness_plot, title = "")
 plot!(lcbdtr_plot, title = "", clim = (-Inf, Inf), cbartitle = "Relative LCBD value")
 plot!(rel2d_plot, title = "")
 
 # Export figures
+savefig(plot(richness_rawplot, dpi = 150), joinpath("docs", "article", "fig", "richness-raw.png"))
 savefig(plot(richness_plot, dpi = 150), joinpath("docs", "article", "fig", "richness.png"))
 savefig(plot(lcbdtr_plot, dpi = 150), joinpath("docs", "article", "fig", "lcbd.png"))
 savefig(plot(rel2d_plot, dpi = 150), joinpath("docs", "article", "fig", "relationship.png"))
-
