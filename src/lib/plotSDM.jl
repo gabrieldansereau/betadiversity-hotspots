f9a74d100363327def8e70b9cdf363dc34c4861f
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-07-30 13:23:41 -0400

    :laughing: remove 'print(LOL XD)'

--- src/lib/plotSDM.jl
+++ src/lib/plotSDM.jl
@@ -57,7 +57,6 @@ function plotSDM(layer::SimpleSDMLayer; scatter::Bool=false, occ=nothing, kw...)
 
     return sdm_plot
 end
-# plotSDM(layer::SimpleSDMLayer) = print("LOL XD")
 
 function plotSDM2(layer::SimpleSDMLayer; kw...)
     ## Arguments
