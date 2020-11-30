53d6ca0edca2d5abd66a77c60bc36742f1512379
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2020-11-30 18:19:50 -0500

    :wrench: remove deprecated DataFrame! syntax

--- src/03c_predictions_bart.jl
+++ src/03c_predictions_bart.jl
@@ -9,13 +9,12 @@ end
 
 
 ## Load predictions from CSV
-results  = DataFrame!(CSV.File(joinpath("data", "proc", "bart_summaries.csv")))
-varimps  = DataFrame!(CSV.File(joinpath("data", "proc", "bart_varimps.csv")))
-pred_df  = DataFrame!(CSV.File(joinpath("data", "proc", "bart_predictions_prob.csv"), missingstrings = ["NA"]))
-lower_df = DataFrame!(CSV.File(joinpath("data", "proc", "bart_predictions_lower.csv"), missingstrings = ["NA"]))
-upper_df = DataFrame!(CSV.File(joinpath("data", "proc", "bart_predictions_upper.csv"), missingstrings = ["NA"]))
-pres_df  = DataFrame!(CSV.File(joinpath("data", "proc", "bart_predictions_pres.csv"), missingstrings = ["NA"]))
-
+results  = DataFrame(CSV.File(joinpath("data", "proc", "bart_summaries.csv")))
+varimps  = DataFrame(CSV.File(joinpath("data", "proc", "bart_varimps.csv")))
+pred_df  = DataFrame(CSV.File(joinpath("data", "proc", "bart_predictions_prob.csv"), missingstrings = ["NA"]))
+lower_df = DataFrame(CSV.File(joinpath("data", "proc", "bart_predictions_lower.csv"), missingstrings = ["NA"]))
+upper_df = DataFrame(CSV.File(joinpath("data", "proc", "bart_predictions_upper.csv"), missingstrings = ["NA"]))
+pres_df  = DataFrame(CSV.File(joinpath("data", "proc", "bart_predictions_pres.csv"), missingstrings = ["NA"]))
 
 ## Create Y matrices
 
