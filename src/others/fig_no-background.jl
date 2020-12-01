d1b8d52448725d2d218e08a0c9712471e4791774
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2020-12-01 18:11:58 -0500

    :sparkles: maps without background

---
+++ src/others/fig_no-background.jl
@@ -0,0 +1,25 @@
+outcome = "bart"
+outcome = "raw"
+include(joinpath(pwd(), "src", "04_analysis.jl"))
+
+coords = (left = -145.0, right = -50.0, bottom = 20.0, top = 75.0)
+temp = worldclim(1)[coords]
+temp5 = worldclim(1, resolution = 5.0)[coords]
+
+testplot = plotSDM2(lcbd, c = :viridis)
+testplot = plotSDM2(richness, c = :viridis)
+testplot = plotSDM2(temp)
+testplot = plotSDM2(temp5)
+testplot = plot(temp5)
+testplot = plot(temp, c = :lightgrey) |> x -> plot!(richness, c = :viridis, clim = extrema(richness))
+testplot = plot(lcbd, c = :viridis)
+
+plot!(
+      frame = :origin,
+      grid = :none,
+      colorbar = :none,
+      guides = "",
+      bg = :transparent,
+      )
+
+savefig(plot(testplot, dpi = 200), "./fig/testplot.png")
