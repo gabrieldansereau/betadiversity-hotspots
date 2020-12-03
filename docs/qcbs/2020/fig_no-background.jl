e85ddfbaeeeb27d3fafbd4695ae97e0809fff570
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2020-12-03 00:03:29 -0500

    :wrench: reorganize fig script

--- docs/qcbs/2020/fig_no-background.jl
+++ docs/qcbs/2020/fig_no-background.jl
@@ -1,31 +1,49 @@
+# Load data
 outcome = "bart"
 outcome = "raw"
 include(joinpath(pwd(), "src", "04_analysis.jl"))
 
+# Load temperature
 coords = (left = -145.0, right = -50.0, bottom = 20.0, top = 75.0)
 temp = worldclim(1)[coords]
 temp5 = worldclim(1, resolution = 5.0)[coords]
 
+# Testing plots
 testplot = plotSDM2(lcbd, c = :viridis)
 testplot = plotSDM2(richness, c = :viridis)
 testplot = plotSDM2(temp)
 testplot = plotSDM2(temp5)
 testplot = plot(temp5)
-testplot = plot(temp, c = :lightgrey) |> x -> plot!(richness, c = :viridis, clim = extrema(richness))
+testplot = plot(temp, c = :lightgrey) |> x ->
+    plot!(richness, c = :viridis, clim = extrema(richness))
 testplot = plot(lcbd, c = :viridis)
+testplot = plotSDM2(temp, c = :lightgrey)
 
-plot!(testplot,
-      bg_outside = :transparent,
-      # fg = :white
-      )
-
-plot!(
+## Remove background
+# Custom function
+function removebackground!(p; kw...)
+    plot!(p,
       frame = :origin,
-      grid = :none,
       colorbar = :none,
       guides = "",
-      bg = :transparent,
+          grid = :none,
+          bg = :transparent;
+          kw...
       )
+    return p
+end
 
+# Remove background
+removebackground!(testplot)
+
+# Save result
 savefig(plot(testplot, dpi = 200), "./docs/qcbs/2020/fig/testplot.png")
-savefig(plot(testplot, dpi = 200), "./docs/qcbs/2020/fig/testplot2.png")
+
+## Keep inside background white
+testplot2 = plot(testplot,
+                 bg_inside = :white,
+                 # bg_outside = :transparent,
+                 # fg = :white
+                 )
+# Save result
+savefig(plot(testplot2, dpi = 200), "./docs/qcbs/2020/fig/testplot2.png")
