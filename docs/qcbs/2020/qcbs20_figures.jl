320aeedf4b7b996956cedb574531babf33c0fa41
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2020-12-04 17:53:28 -0500

    :boom: completely reorganize QCBS20 script & figures ... again

--- docs/qcbs/2020/qcbs20_figures.jl
+++ docs/qcbs/2020/qcbs20_figures.jl
@@ -1,32 +1,32 @@
-# Load data
-outcome = "bart"
-outcome = "raw"
-include(joinpath(pwd(), "src", "04_analysis.jl"))
+## Prepare layers & data
+import Pkg
+Pkg.activate(".")
+@time include(joinpath("..", "..", "..", "src", "required.jl")) # best way for Intellisense
 
 # Load temperature
 coords = (left = -145.0, right = -50.0, bottom = 20.0, top = 75.0)
-temp = worldclim(1)[coords]
+temp_full = worldclim(1)[coords]
 # temp5 = worldclim(1, resolution = 5.0)[coords]
 
 # Clip to QC
 coords_qc = (left = -82.5, right = -51.0,
              bottom = 41.0, top = 71.0)
-temp_qc = temp[coords_qc]
+temp_qc = temp_full[coords_qc]
+# Verify dimensions
 plot(temp_qc)
 # Set Greenland values to nothing
-size(temp_qc)
 xmin = SimpleSDMLayers._match_longitude(temp_qc, -55.0)
 xmax = SimpleSDMLayers._match_longitude(temp_qc, coords_qc.right)
 ymin = SimpleSDMLayers._match_latitude(temp_qc, 60.0)
 ymax = SimpleSDMLayers._match_latitude(temp_qc, coords_qc.top)
 temp_qc.grid[ymin:ymax, xmin:xmax] .= nothing
 plot(temp_qc)
-# Default to QC values
-temp_full = copy(temp)
-temp = temp_qc
 
-# Temperature map
-temp_plot = plot(temp_qc)
+## Create maps for QC
+qc_inferno = plot(temp_qc)
+qc_lightgrey = plot(temp_qc, c = :lightgrey)
+qc_viridis = plot(temp_qc, c = :viridis)
+qc_white = plot(temp_qc, c = :white)
 
 ## Remove background
 # Custom function
@@ -36,40 +36,34 @@ function removebackground!(p; kw...)
           colorbar = :none,
           guides = "",
           grid = :none,
-          bg = :transparent;
+          bg = :transparent,
+          bg_inside = :transparent;
           kw...
           )
     return p
 end
 
 # Remove background
-removebackground!(temp_plot)
+removebackground!(qc_inferno)
+removebackground!(qc_lightgrey)
+removebackground!(qc_viridis)
+removebackground!(qc_white)
+
 # Save results
-savefig(plot(temp_plot, dpi = 200), "./docs/qcbs/2020/fig/qc_none_inferno.png")
+savefig(plot(qc_inferno,   dpi = 200), "./docs/qcbs/2020/fig/qc_no-bg_inferno.png")
+savefig(plot(qc_lightgrey, dpi = 200), "./docs/qcbs/2020/fig/qc_no-bg_lightgrey.png")
+savefig(plot(qc_viridis,   dpi = 200), "./docs/qcbs/2020/fig/qc_no-bg_viridis.png")
+savefig(plot(qc_white,     dpi = 200), "./docs/qcbs/2020/fig/qc_no-bg_white.png")
 
-## Paint it white
-# White inside background
-temp_white = removebackground!(plot(temp_qc), bg_inside = :white)
+# Paint it white
+plot!(qc_inferno, bg_inside = :white)
+plot!(qc_lightgrey, bg_inside = :white)
+plot!(qc_viridis, bg_inside = :white)
+
 # Save results
-savefig(plot(temp_white, dpi = 200), "./docs/qcbs/2020/fig/qc_inside_inferno.png")
-
-## Paint it viridis
-# Viridis colorpalette
-temp_viridis = removebackground!(plot(temp_qc, c = :viridis))
-savefig(plot(temp_viridis, dpi = 200), "./docs/qcbs/2020/fig/qc_none_viridis.png")
-# White inside
-temp_viridis_white = removebackground!(plot(temp_qc, c = :viridis), bg_inside = :white)
-savefig(plot(temp_viridis_white, dpi = 200), "./docs/qcbs/2020/fig/qc_inside_viridis.png")
-
-## Create empty background map
-empty_plot = plot(temp_qc, c = :lightgrey)
-removebackground!(empty_plot)
-savefig(plot(empty_plot, dpi = 200), "./docs/qcbs/2020/fig/qc_none_lightgrey.png")
-savefig(plot(empty_plot, dpi = 200, bg_inside = :white), "./docs/qcbs/2020/fig/qc_inside_lightgrey.png")
-# No background white
-empty_plot_white = removebackground!(plot(temp_qc, c = :white))
-savefig(plot(empty_plot_white, dpi = 200), "./docs/qcbs/2020/fig/qc_none_white.png")
-
+savefig(plot(qc_inferno,   dpi = 200), "./docs/qcbs/2020/fig/qc_inside-bg_inferno.png")
+savefig(plot(qc_lightgrey, dpi = 200), "./docs/qcbs/2020/fig/qc_inside-bg_lightgrey.png")
+savefig(plot(qc_viridis,   dpi = 200), "./docs/qcbs/2020/fig/qc_inside-bg_viridis.png")
 
 ## Draw rectangle over subareas
 # Get coordinates
