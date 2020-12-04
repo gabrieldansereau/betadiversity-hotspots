a329301c9213524f954748db85e1c97e07923646
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2020-12-04 16:06:43 -0500

    :truck: rename & reorganize figures

--- docs/qcbs/2020/fig_no-background.jl
+++ docs/qcbs/2020/fig_no-background.jl
@@ -12,7 +12,7 @@ temp = worldclim(1)[coords]
 coords_qc = (left = -82.5, right = -51.0,
              bottom = 41.0, top = 71.0)
 temp_qc = temp[coords_qc]
-plot(temp[coords_qc])
+plot(temp_qc)
 # Set Greenland values to nothing
 size(temp_qc)
 xmin = SimpleSDMLayers._match_longitude(temp_qc, -55.0)
@@ -26,7 +26,7 @@ temp_full = copy(temp)
 temp = temp_qc
 
 # Temperature map
-temp_plot = plot(temp)
+temp_plot = plot(temp_qc)
 
 ## Remove background
 # Custom function
@@ -45,30 +45,30 @@ end
 # Remove background
 removebackground!(temp_plot)
 # Save results
-savefig(plot(temp_plot, dpi = 200), "./docs/qcbs/2020/fig/temp_no-bg.png")
+savefig(plot(temp_plot, dpi = 200), "./docs/qcbs/2020/fig/qc_none_inferno.png")
 
 ## Paint it white
 # White inside background
-temp_white = removebackground!(plot(temp), bg_inside = :white)
+temp_white = removebackground!(plot(temp_qc), bg_inside = :white)
 # Save results
-savefig(plot(temp_white, dpi = 200), "./docs/qcbs/2020/fig/temp_white-in.png")
+savefig(plot(temp_white, dpi = 200), "./docs/qcbs/2020/fig/qc_inside_inferno.png")
 
 ## Paint it viridis
 # Viridis colorpalette
-temp_viridis = removebackground!(plot(temp, c = :viridis))
-savefig(plot(temp_viridis, dpi = 200), "./docs/qcbs/2020/fig/temp_no-bg_viridis.png")
+temp_viridis = removebackground!(plot(temp_qc, c = :viridis))
+savefig(plot(temp_viridis, dpi = 200), "./docs/qcbs/2020/fig/qc_none_viridis.png")
 # White inside
-temp_viridis_white = removebackground!(plot(temp, c = :viridis), bg_inside = :white)
-savefig(plot(temp_viridis_white, dpi = 200), "./docs/qcbs/2020/fig/temp_white-in_viridis.png")
+temp_viridis_white = removebackground!(plot(temp_qc, c = :viridis), bg_inside = :white)
+savefig(plot(temp_viridis_white, dpi = 200), "./docs/qcbs/2020/fig/qc_inside_viridis.png")
 
 ## Create empty background map
-empty_plot = plot(temp, c = :lightgrey)
+empty_plot = plot(temp_qc, c = :lightgrey)
 removebackground!(empty_plot)
-savefig(plot(empty_plot, dpi = 200), "./docs/qcbs/2020/fig/empty_no-bg.png")
-savefig(plot(empty_plot, dpi = 200, bg_inside = :white), "./docs/qcbs/2020/fig/empty_white.png")
+savefig(plot(empty_plot, dpi = 200), "./docs/qcbs/2020/fig/qc_none_lightgrey.png")
+savefig(plot(empty_plot, dpi = 200, bg_inside = :white), "./docs/qcbs/2020/fig/qc_inside_lightgrey.png")
 # No background white
-empty_plot_white = removebackground!(plot(temp, c = :white))
-savefig(plot(empty_plot_white, dpi = 200), "./docs/qcbs/2020/fig/empty_no-bg_white.png")
+empty_plot_white = removebackground!(plot(temp_qc, c = :white))
+savefig(plot(empty_plot_white, dpi = 200), "./docs/qcbs/2020/fig/qc_none_white.png")
 
 
 ## Draw rectangle over subareas
@@ -93,7 +93,7 @@ rect_SW = rectangle_from_coords(coords_SW.left,  coords_SW.bottom,
                                 coords_SW.right, coords_SW.top)
 
 # Plot subarea rectangles
-rectplot = plot(temp, c = :lightgrey, cb = :none)
+rectplot = plot(temp_full, c = :lightgrey, cb = :none)
 plot!(rectplot, rect_NE[:, 1], rect_NE[:, 2], label = "NE subarea")
 plot!(rectplot, rect_SW[:, 1], rect_SW[:, 2], label = "SW subarea")
 savefig(plot(rectplot, dpi = 200), "./docs/qcbs/2020/fig/subarea-map.png")
