d662e223e689e1935cb1db9998a78cc696d55f85
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-03-22 23:37:09 -0400

    🚧 messy GLM attempts

---
+++ src/07_raw-sdm-comparison.jl
@@ -0,0 +1,187 @@
+import Pkg; Pkg.activate(".")
+@time include("required.jl")
+
+outcome = "raw"
+include("04_analysis.jl")
+
+raw = (Y = Y,
+       richness = richness,
+       richness_plot = richness_plot,
+       lcbd = lcbd,
+       beta_total = beta_total,
+       lcbd_plot = lcbdtr_plot,
+       rel_plot = rel2d_plot
+       )
+
+outcome = "bart"
+include("04_analysis.jl")
+
+sdm = (Y = Y,
+       richness = richness,
+       richness_plot = richness_plot,
+       lcbd = lcbd,
+       beta_total = beta_total,
+       lcbd_plot = lcbdtr_plot,
+       rel_plot = rel2d_plot
+       )
+
+# Plots
+lims_richness = extrema(mapreduce(collect, vcat, [raw.richness, sdm.richness]))
+lims_lcbd = extrema(mapreduce(collect, vcat, [raw.lcbd, sdm.lcbd]))
+
+plot(raw.richness_plot, sdm.richness_plot,
+     raw.lcbd_plot, sdm.lcbd_plot,
+     clim = [lims_richness lims_richness lims_lcbd lims_lcbd],
+     title = ["a" "b" "c" "d"],
+     titleloc = :left,
+     size = (850, 600), 
+     dpi = 200)
+
+if (@isdefined save_figures) && save_figures == true
+    mspath = abspath("..", "ms_betadiversity_hotspots")
+    savefig(joinpath(mspath, "figures", "combined-maps.png"))
+end
+
+## Correlation
+# Prepare data
+results = DataFrame([raw.richness, raw.lcbd, sdm.richness, sdm.lcbd])
+rename!(results, 
+        :x1 => :richness_raw, :x2 => :lcbd_raw, 
+        :x3 => :richness_sdm, :x4 => :lcbd_sdm)
+allowmissing!(results, Not([:latitude, :longitude]))
+for col in eachcol(results)
+    replace!(col, nothing => missing)
+end
+dropmissing!(results)
+results = convert.(Float64, results)
+
+# Check correlation
+cor(results.richness_raw, results.richness_sdm)
+cor(results.lcbd_raw, results.lcbd_sdm)
+
+histogram(results.richness_raw, title = "richness_raw", legend = :none)
+histogram(results.richness_sdm, title = "richness_sdm", legend = :none)
+
+histogram(results.lcbd_raw, title = "lcbd_raw", legend = :none)
+histogram(results.lcbd_sdm, title = "lcbd_sdm", legend = :none)
+
+
+# Linear Regression
+using GLM
+lm_richness = lm(@formula(richness_sdm ~ richness_raw), results)
+lm_lcbd = lm(@formula(lcbd_sdm ~ lcbd_raw), results)
+
+coef(lm_richness)
+deviance(lm_richness)
+dof_residual(lm_richness)
+r2(lm_richness)
+stderror(lm_richness)
+vcov(lm_richness)
+predict(lm_richness)
+
+glm_richness = glm(@formula(richness_sdm ~ richness_raw), results, Poisson())
+negb_richness = negbin(@formula(richness_sdm ~ richness_raw), results, LogLink())
+glm_lcbd = glm(@formula(richness_sdm ~ richness_raw), results, Gamma())
+
+mean(results.richness_raw)
+std(results.richness_raw)
+variation(results.richness_raw)
+var(results.richness_raw)/mean(results.richness_raw)
+
+mean(results.richness_sdm)
+std(results.richness_sdm)
+variation(results.richness_sdm)
+
+describe(results, :mean, :std)
+
+# Regression in R
+@rput results
+
+R"""
+lm_richness <- lm(richness_sdm ~ richness_raw, data = results)
+lm_lcbd <- lm(lcbd_sdm ~ lcbd_raw, data = results)
+
+summary(lm_richness)
+summary(lm_lcbd)
+
+plotlm <- function(model) {
+    opar <- par(mfrow=c(2,2))
+    plot(model)
+    par(opar)    
+}
+
+plotlm(lm_richness)
+plotlm(lm_lcbd)
+
+plot(results$richness_raw, results$richness_sdm)
+abline(lm_richness, col = "red")
+
+plot(results$lcbd_raw, results$lcbd_sdm)
+abline(lm_lcbd, col = "red")
+
+hist(results$richness_raw)
+hist(results$richness_sdm)
+hist(results$lcbd_raw)
+hist(results$lcbd_sdm)
+
+# loglm_richness <- lm(log10(richness_sdm) ~ log10(richness_raw), data = results)
+# summary(loglm_richness)
+# plotlm(loglm_richness)
+
+# loglm_lcbd <- lm(log10(lcbd_sdm) ~ log10(lcbd_raw), data = results)
+# summary(loglm_lcbd)
+# plotlm(loglm_lcbd)
+
+# Poisson
+glm_richness <- glm(richness_sdm ~ richness_raw, data = results, family = poisson)
+summary(glm_richness)
+# Overdispersion (Null deviance ~3-4x degrees of freedom)
+# Quasi-Poisson
+glm_richness <- glm(richness_sdm ~ richness_raw, data = results, family = quasipoisson)
+summary(glm_richness)
+# Negative binomial
+library(MASS)
+glm_nb_richness <- glm.nb(richness_sdm ~ richness_raw, data = results)
+summary(glm_nb_richness)
+
+glm_lcbd <- glm(lcbd_sdm ~ lcbd_raw, data = results, family = Gamma)
+summary(glm_lcbd)
+
+richness_res <- residuals(glm_nb_richness)
+lcbd_res <- residuals(glm_lcbd)
+
+"""
+
+@rget richness_res lcbd_res
+
+# Check residuals
+results.richness_res = richness_res
+results.lcbd_res = lcbd_res
+
+richres_layer = SimpleSDMResponse(results, :richness_res, similar(raw.richness), 
+                                  latitude = :latitude, longitude = :longitude)
+lcbdres_layer = SimpleSDMResponse(results, :lcbd_res, similar(raw.lcbd), 
+                                  latitude = :latitude, longitude = :longitude)
+
+plotSDM2(richres_layer, c = :PuOr, dpi = 200)
+plotSDM2(lcbdres_layer, c = :PuOr, dpi = 200)
+
+histogram(richres_layer)
+histogram(lcbdres_layer)
+histogram2d(richres_layer, lcbdres_layer)
+
+# Julia residuals
+histogram(residuals(lm_richness))
+histogram(residuals(lm_lcbd))
+
+results.richness_res_jl = residuals(lm_richness)
+results.lcbd_res_jl = residuals(lm_lcbd)
+
+richresjl_layer = SimpleSDMResponse(results, :richness_res_jl, similar(raw.richness), 
+                                  latitude = :latitude, longitude = :longitude)
+lcbdresjl_layer = SimpleSDMResponse(results, :lcbd_res_jl, similar(raw.lcbd), 
+                                  latitude = :latitude, longitude = :longitude)
+
+plotSDM2(richresjl_layer, c = :PuOr, dpi = 200)
+plotSDM2(lcbdresjl_layer, c = :PuOr, dpi = 200)
+
