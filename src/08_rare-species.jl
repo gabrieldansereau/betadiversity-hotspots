c0cad28d2df2adbde0ad4aa83ecef7fb5d651d9c
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-04-08 17:49:54 -0400

    🚧 subareas & thresholds

--- src/08_rare-species.jl
+++ src/08_rare-species.jl
@@ -35,7 +35,7 @@ rarespecies = getrarespecies(occupancy_percentage, 0.4)
 rarespecies_NE = getrarespecies(occupancy_percentage_NE, 0.4)
 rarespecies_SW = getrarespecies(occupancy_percentage_SW, 0.4)
 # Percentage of rarespecies
-rarespecies_percentage = mean(rarespecies)
+rarespecies_percentage = mean(skipmissing(rarespecies))
 rarespecies_percentage_NE = mean(skipmissing(rarespecies_NE))
 rarespecies_percentage_SW = mean(skipmissing(rarespecies_SW))
 
@@ -44,6 +44,25 @@ get_eusrr(richness, lcbd) = corspearman(collect(richness), collect(lcbd))
 eusrr = get_eusrr(richness, lcbd)
 eusrr_NE = get_eusrr(richness_NE, lcbd_NE)
 eusrr_SW = get_eusrr(richness_SW, lcbd_SW)
+
+# Wrap as function
+function get_rarespecies_percentage(Y, threshold)
+    occupancy_percentage = getoccupancy_percentage(Y)
+    rarespecies = getrarespecies(occupancy_percentage, threshold)
+    rarespecies_percentage = mean(skipmissing(rarespecies))
+    return rarespecies_percentage
+end
+
+# Validate
+get_rarespecies_percentage(Y, 0.4)
+rarespecies_percentage
+
+# Thresholds variation only
+thresholds = [0.1, 0.2, 0.3, 0.4, 0.5]
+[get_rarespecies_percentage(Y, t) for t in thresholds]
+# Thresholds and region 
+Ys = [Y, Y_NE, Y_SW]
+rarespecies_matrix = [get_rarespecies_percentage(y, t) for t in thresholds, y in Ys]
 
 # Plot EUSRR ~ rare species percentage
 # Similar to Fig. 3 of Yao et al. 2021
@@ -93,5 +112,26 @@ scatter(permutedims([rarespecies_percentage, rarespecies_percentage_NE, rarespec
         legendtitle = "Regions", legendtitlefontsize = 9,
         ) |>
         x -> hline!(x, [0.0], style = :dash, c = :grey, label = :none)
-# EUSRR relative whatever percentage of rare species?
+# EUSRR negative whatever subarea & percentage of rare species?
 savefig(joinpath("fig", outcome, "08_$(outcome)_rare-species_eusrr.png"))
+
+# Subareas & thresholds
+scatter(rarespecies_matrix, 
+        [eusrr eusrr_NE eusrr_SW],
+        xlabel = "Percentage of rare species (%)",
+        ylabel = "EUSRR",
+        xlim = (0.0, 1.0), ylim = (-1.0, 1.0),
+        label = ["Total" "NE" "SW"],
+        legendtitle = "Regions", legendtitlefontsize = 9,
+        ) |>
+        x -> hline!(x, [0.0], style = :dash, c = :grey, label = :none)
+# EUSRR negative whatever subarea, percentage of rare species & threshold
+# Whatever the threshold, EUSRR negative
+savefig(joinpath("fig", outcome, "08_$(outcome)_rare-species_eusrr_thresholds.png"))
+
+plot(thresholds, rarespecies_matrix,
+     xlabel = "Rare species threshold",
+     ylabel = "Rare species percentage",
+     labels = ["Total" "NE" "SW"],
+     legend = :bottomright)
+savefig(joinpath("fig", outcome, "08_$(outcome)_rare-species_eusrr_thresholds.png"))
