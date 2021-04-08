82a15b34a6df6a488ad9c389da0ec2ccac556bef
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-04-08 11:53:24 -0400

    🚧 rare species threshold difference

--- src/08_rare-species.jl
+++ src/08_rare-species.jl
@@ -19,20 +19,23 @@ occupancy_plot = scatter(occupancy, formatter = :plain, legend = :none,
 # Occupancy percentage
 occupancy_percentage = occupancy ./ size(Yobs, 1)
 # Classify as rare species
-rarespecies = occupancy_percentage .< 0.4
+thresholds = [0.1, 0.2, 0.3, 0.4, 0.5]
+rarespecies = [occupancy_percentage .< t for t in thresholds]
 # Number of rarespecies
-sum(rarespecies)
+sum.(rarespecies)
 # Percentage of rarespecies
-rarespecies_percentage = sum(rarespecies)/length(rarespecies)
+rarespecies_percentage = sum.(rarespecies)./length.(rarespecies)
 
 # LCBD-richness correlation
 eusrr = corspearman(collect(richness), collect(lcbd))
 
 # Plot EUSRR ~ rare species percentage
 # Similar to Fig. 3 of Yao et al. 2021
-scatter([rarespecies_percentage], [eusrr],
+scatter(permutedims(rarespecies_percentage), repeat([eusrr], 5),
         xlabel = "Percentage of rare species (%)",
         ylabel = "EUSRR",
-        xlim = (0.50, 1.0), ylim = (-1.0, 1.0),
-        label = :none)
+        xlim = (0.40, 1.0), ylim = (-1.0, 1.0),
+        label = permutedims(string.(thresholds)),
+        legendtitle = "Thresholds", legendtitlefontsize = 9,
+        )
 hline!([0.0], style = :dash, c = :grey, label = :none)
