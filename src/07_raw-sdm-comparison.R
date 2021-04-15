667d67a0d2cc0b580137a1e2c917c2384f83440f
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-04-15 16:32:11 -0400

    🚧 play with beta regression

--- src/07_raw-sdm-comparison.R
+++ src/07_raw-sdm-comparison.R
@@ -5,6 +5,7 @@ results
 
 lm_richness <- lm(richness_sdm ~ richness_raw, data = results)
 lm_lcbd <- lm(lcbd_sdm ~ lcbd_raw, data = results)
+lm_lcbdnr <- lm(lcbdnr_sdm ~ lcbdnr_raw, data = results)
 
 summary(lm_richness)
 summary(lm_lcbd)
@@ -27,28 +28,25 @@ plotlm(lm_richness) # not met
 plotlm(lm_lcbd) # not met
 
 # Plot relation
-lm_richness %>% 
-    augment(type = "response") %>% 
-    rename(.y = 1, .x = 2) %>% 
-    ggplot(aes(x = .x, y = .y)) +
-        geom_point() +
-        # geom_abline(
-        #     intercept = coef(lm_richness)[1],
-        #     slope = coef(lm_richness)[2]
-        # )
-        geom_line(
-            aes(y = .fitted),
-            color = "red"
-        )
-lm_lcbd %>% 
-    augment(type = "response") %>% 
-    rename(.y = 1, .x = 2) %>% 
-    ggplot(aes(x = .x, y = .y)) +
-    geom_point() +
-    geom_line(
-        aes(y = .fitted),
-        color = "red"
-    )
+plotfit <- function(model) {
+    model %>% 
+        augment(type.predict = "response") %>% 
+        rename(.y = 1, .x = 2) %>% 
+        ggplot(aes(x = .x, y = .y)) +
+            geom_point() +
+            # geom_abline(
+            #     intercept = coef(lm_richness)[1],
+            #     slope = coef(lm_richness)[2]
+            # )
+            geom_line(
+                aes(y = .fitted),
+                color = "red"
+            ) +
+            labs(y = names(model$model)[1], x = names(model$model)[2])
+}
+plotfit(lm_richness)
+plotfit(lm_lcbd)
+plotfit(lm_lcbdnr)
 
 # Check distribution
 hist(results$richness_raw)
@@ -63,30 +61,15 @@ glm_richness <- glm(richness_sdm ~ richness_raw, data = results, family = poisso
 summary(glm_richness)
 tidy(glm_richness)
 glance(glm_richness)
-# Overdispersion (Null deviance ~3-4x degrees of freedom)
+glance(glm_richness) %>% 
+    transmute(dispersion = deviance/df.residual)
+# Overdispersion (residual deviance ~3-4x residual degrees of freedom)
+# Too much dispersion for Poisson
+var(results$richness_raw)/mean(results$richness_raw)
+var(results$richness_sdm)/mean(results$richness_sdm)
+# Variance way bigger than mean, too big for Poisson distribution
 
-poisregfun = function(a, b) {
-    Y.pred = exp(a + b * x)
-    -sum(dpois(y, lambda = Y.pred, log = TRUE))
-}
-
-
-glm_richness %>% 
-    augment(type.predict = "response") %>% 
-    rename(.y = 1, .x = 2) %>% 
-    ggplot(aes(x = .x, y = .y)) +
-        geom_point() +
-        geom_line(
-            aes(y = .fitted),
-            color = "red"
-        )
-
-glm_richness %>% 
-    augment(type.predict = "response") %>% 
-    rename(.y = 1, .x = 2) %>% 
-    ggplot(aes(x = .x, y = .y)) +
-        geom_point() +
-        geom_smooth(method = "glm", se = TRUE, method.args = list(family = "poisson"))
+plotfit(glm_richness)
 
 # install.packages("ggiraph")
 # install.packages("ggiraphExtra")
@@ -131,6 +114,53 @@ glm_lcbd <- glm(lcbd_sdm ~ lcbd_raw, data = results, family = Gamma)
 summary(glm_lcbd)
 tidy(glm_lcbd)
 glance(glm_lcbd)
+
+cv(results$lcbd_raw)
+cv(results$lcbd_sdm)
+# Gamma supposedly useful when coefficient of variation > 50%,
+# which is not the case now
+# Maybe Gamma is not right...
+
+AIC(glm_lcbd, lm_lcbd)
+# Gamma GLM has lower AIC
+
+plotfit(glm_lcbd)
+
+# Beta regression attempt
+library(betareg)
+beta_lcbd <- betareg(lcbd_sdm ~ lcbd_raw, data = results, link = "logit")
+beta_lcbdnr <- betareg(lcbdnr_sdm ~ lcbdnr_raw, data = results)
+summary(beta_lcbd)
+tidy(beta_lcbd)
+glance(beta_lcbd)
+plotlm(beta_lcbd)
+plotfit(beta_lcbd)q
+
+lm_uniqueness <- lm(lcbd_raw ~ richness_raw, data = results)
+summary(lm_uniqueness)
+plotlm(lm_uniqueness)
+AIC(lm_uniqueness)
+
+results <- results %>% 
+    mutate(
+        richness_raw_std = richness_raw/max(richness_raw),
+        richness_sdm_std = richness_sdm/max(richness_sdm)
+    )
+
+beta_uniqueness <- betareg(lcbd_sdm ~ richness_sdm, data = results)
+summary(beta_uniqueness)
+plotlm(beta_uniqueness)
+AIC(beta_uniqueness)
+
+beta_uniqueness %>% 
+    augment(type.predict = "response") %>% 
+    rename(.y = 1, .x = 2) %>% 
+    ggplot(aes(x = .x, y = .y)) +
+        geom_point() +
+        geom_line(
+            aes(y = .fitted),
+            color = "red"
+        )
 
 # Get residuals
 residuals_df <- tibble(
