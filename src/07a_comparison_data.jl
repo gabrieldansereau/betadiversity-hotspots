7670ad8c79cd7d706b386d1f4bd19e512e98a246
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-05-03 11:40:58 -0400

    ✨ recalculate LCBD values for same sites

--- src/07a_comparison_data.jl
+++ src/07a_comparison_data.jl
@@ -39,6 +39,35 @@ sdm = (
     rel_plot = rel2d_plot
 )
 
+## Recalculate LCBD values on same sites
+inds_common = intersect(_indsobs(raw.Y), _indsobs(sdm.Y))
+function recalculate_lcbd(Y, layer, inds_common)
+    Ycommon = copy(Y)
+    Ycommon[Not(inds_common), :] .= nothing
+    lcbd_common = calculate_lcbd(Ycommon, layer; relative = false)
+    return lcbd_common
+end
+lcbdcom_raw = recalculate_lcbd(raw.Y, raw.lcbd, inds_common)
+lcbdcom_sdm = recalculate_lcbd(sdm.Y, sdm.lcbd, inds_common)
+plotSDM2(lcbdcom_raw, c = :viridis)
+plotSDM2(lcbdcom_sdm, c = :viridis)
+
+# Add to NamedTuples
+raw = (raw..., lcbdcom = lcbdcom_raw)
+sdm = (sdm..., lcbdcom = lcbdcom_sdm)
+
+## Tests
+# isequal([sdm.richness[i] for i in inds_common], results.richness_sdm)
+# isequal([raw.richness[i] for i in inds_common], results.richness_raw)
+# isequal([sdm.lcbd[i] for i in inds_common], results.lcbd_sdm)
+# isequal([raw.lcbd[i] for i in inds_common], results.lcbd_raw)
+# Ycommon = copy(raw.Y)
+# Ycommon[Not(inds_common), :] .= nothing
+# isequal(collect(calculate_richness(Ycommon, raw.richness)), raw.richness.grid[inds_common])
+# calculate_lcbd(Ycommon, raw.lcbd; relative = false) |>
+#     x -> plotSDM2(x, c = :viridis)
+# plotSDM2(raw.lcbd; relative = false, c = :viridis)
+
 # Export to JLD2
 jld_path = joinpath("data", "jld2", "comparison-results.jld2")
 @save jld_path raw sdm
