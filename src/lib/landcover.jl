50df1f5ce1800ae2f853d196020d764091253f1b
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-05-27 13:46:49 -0400

    🔨 inverse single integer & range loading calls

--- src/lib/landcover.jl
+++ src/lib/landcover.jl
@@ -2,67 +2,57 @@
 
 struct Copernicus end
 
-# function SimpleSDMPredictor(::Type{Copernicus}, ::Type{LandCover}, layer::Integer=1; kwargs...)
-#     file = _get_raster(EarthEnv, LandCover, layer, full)
-#     return geotiff(SimpleSDMPredictor, file; kwargs...)
-# end
-
-# function SimpleSDMPredictor(::Type{EarthEnv}, ::Type{LandCover}, layers::AbstractArray; kwargs...)
-#     @assert eltype(layers) <: Integer
-#     return [SimpleSDMPredictor(EarthEnv, LandCover, l; kwargs...) for l in layers]
-# end
-
 import SimpleSDMLayers: SimpleSDMPredictor
-
-# Landcover variables loading
-function SimpleSDMPredictor(::Type{Copernicus}, ::Type{LandCover}, layers::Vector{Int64}; resolution::AbstractFloat=10.0, path::AbstractString=joinpath("assets", "landcover/"))
-    ## Get file paths
-    # List files in path
-    lc_files = readdir("$(path)")
-    # Filter for selected resolution
-    filter!(x -> occursin.("$(Int(resolution))m.tif", x), lc_files)
-    # Create paths for selected layers only
-    paths = joinpath.(path, lc_files)[layers]
-
-    ## Load data
-    # Read raster dataset
-    datasets = [ArchGDAL.read(p) for p in paths]
-    # Read raster values from band 1 (only band in this case)
-    values = [ArchGDAL.read(d, 1) for d in datasets]
-    # Convert from UInt8 to Float64
-    values_int = [convert.(Float64, v) for v in values]
-
-    ## Preliminary manipulations
-    # Reverse rows and permute dimensions
-    landcover_mat = [permutedims(v[:,end:-1:1]) for v in values_int]
-    # Replace 255 (default no data values) by nothing
-    landcover_mat = convert.(Array{Union{Float32, Nothing}}, landcover_mat)
-    [replace!(l, 255 => nothing) for l in landcover_mat]
-
-    # Fill missing latitudes with nothing (latitude extent is only (-60,80) for landcover data)
-    nlat, nlon = size(landcover_mat[1])
-    slim, nlim = abs(-60), 80
-    res = Int64(nlat/(nlim+slim))
-    south_nothings = fill(nothing, ((90-slim)*res, nlon))
-    north_nothings = fill(nothing, ((90-nlim)*res, nlon))
-    landcover_grids = [vcat(south_nothings, l, north_nothings) for l in landcover_mat]
-
-    # Convert to SimpleSDMLayers
-    landcover_layers = SimpleSDMPredictor.(landcover_grids, -180.0, 180.0, -90.0, 90.0)
-
-    return landcover_layers
-end
 
 """
     SimpleSDMPredictor(Copernicus, LandCover, layer::Int64; x...)
 
 Return a single landcover layer.
 """
-SimpleSDMPredictor(::Type{Copernicus}, ::Type{LandCover}, layer::Int64; x...) = SimpleSDMPredictor(Copernicus, LandCover, [layer]; x...)[1]
+function SimpleSDMPredictor(::Type{Copernicus}, ::Type{LandCover}, layer::Integer=1; resolution::Float64=10.0, path::AbstractString=joinpath("assets", "landcover"), kwargs...)
+    ## Get file path
+    # List files in path
+    lc_files = readdir(path)
+    # Filter for selected resolution
+    filter!(x -> occursin.("$(Int(resolution))m.tif", x), lc_files)
+    # Create path for selected layer only
+    p = joinpath.(path, lc_files)[layer]
+
+    ## Load data
+    # Read raster dataset
+    d = ArchGDAL.read(p)
+    # Read raster values from band 1 (only band in this case)
+    v = ArchGDAL.read(d, 1)
+    # Convert from UInt8 to Float64
+    v = convert.(Float64, v)
+
+    ## Preliminary manipulations
+    # Reverse rows and permute dimensions
+    landcover_mat = permutedims(v[:,end:-1:1])
+    # Replace 255 (default no data values) by nothing
+    landcover_mat = convert(Array{Union{Float32, Nothing}}, landcover_mat)
+    replace!(landcover_mat, 255 => nothing)
+
+    # Fill missing latitudes with nothing (latitude extent is only (-60,80) for landcover data)
+    nlat, nlon = size(landcover_mat)
+    slim, nlim = abs(-60), 80
+    res = Int64(nlat/(nlim+slim))
+    south_nothings = fill(nothing, ((90-slim)*res, nlon))
+    north_nothings = fill(nothing, ((90-nlim)*res, nlon))
+    landcover_grids = vcat(south_nothings, landcover_mat, north_nothings)
+
+    # Convert to SimpleSDMLayers
+    landcover_layers = SimpleSDMPredictor(landcover_grids, -180.0, 180.0, -90.0, 90.0)
+
+    return landcover_layers
+end
 
 """
     SimpleSDMPredictor(Copernicus, LandCover, layers::UnitRange{Int64}; x...)
 
 Return a range of landcover layers.
 """
-SimpleSDMPredictor(::Type{Copernicus}, ::Type{LandCover}, layers::UnitRange{Int64}; x...) = SimpleSDMPredictor(Copernicus, LandCover, collect(layers); x...)
+function SimpleSDMPredictor(::Type{Copernicus}, ::Type{LandCover}, layers::AbstractArray; kwargs...)
+    @assert eltype(layers) <: Integer
+    return [SimpleSDMPredictor(Copernicus, LandCover, l; kwargs...) for l in layers]
+end
--- src/others/test_landcover-copernicus-type.jl
+++ src/others/test_landcover-copernicus-type.jl
@@ -1,40 +1,28 @@
-# struct Copernicus end
-
-# function SimpleSDMPredictor(::Type{Copernicus}, ::Type{LandCover}, layer::Integer=1; kwargs...)
-#     file = _get_raster(EarthEnv, LandCover, layer, full)
-#     return geotiff(SimpleSDMPredictor, file; kwargs...)
-# end
-
-# function SimpleSDMPredictor(::Type{EarthEnv}, ::Type{LandCover}, layers::AbstractArray; kwargs...)
-#     @assert eltype(layers) <: Integer
-#     return [SimpleSDMPredictor(EarthEnv, LandCover, l; kwargs...) for l in layers]
-# end
-
 import Pkg; Pkg.activate(".")
 include("../required.jl")
 
 coords = (left = -145.0, right = -50.0, bottom = 20.0, top = 75.0)
 
 # Test loading variables
-lc_vars = landcover(1:10, resolution = 5.0)
-lc_vars = landcover(1:10, resolution = 10.0)
-lc_vars = map(x -> landcover(x, resolution = 10.0)[coords], 1:10)
+# lc_vars = landcover(1:10, resolution = 5.0)
+# lc_vars = landcover(1:10, resolution = 10.0)
+# lc_vars = map(x -> landcover(x, resolution = 10.0)[coords], 1:10)
 
-# Test loading variables
-lc_vars = SimpleSDMPredictor(Copernicus, LandCover, 1:10; resolution = 5.0)
-lc_vars = SimpleSDMPredictor(Copernicus, LandCover, 1:10)
-lc_vars = map(x -> SimpleSDMPredictor(Copernicus, LandCover, x)[coords], 1:10)
+# # Test loading variables
+# lc_vars = SimpleSDMPredictor(Copernicus, LandCover, 1:10; resolution = 5.0)
+# lc_vars = SimpleSDMPredictor(Copernicus, LandCover, 1:10)
+# lc_vars = map(x -> SimpleSDMPredictor(Copernicus, LandCover, x)[coords], 1:10)
 
-# Check compatibility
-lc1 = SimpleSDMPredictor(Copernicus, LandCover, 1)
-wc1 = SimpleSDMPredictor(WorldClim, BioClim, 1)
-SimpleSDMLayers._layers_are_compatible(lc1, wc1) # compatible
+# # Check compatibility
+# lc1 = SimpleSDMPredictor(Copernicus, LandCover, 1)
+# wc1 = SimpleSDMPredictor(WorldClim, BioClim, 1)
+# SimpleSDMLayers._layers_are_compatible(lc1, wc1) # compatible
 
-# lc1 = SimpleSDMPredictor(Copernicus, LandCover, 1; coords...)
-lc1 = SimpleSDMPredictor(Copernicus, LandCover, 1)[coords]
-lc1 = lc1[top = coords.top - stride(lc1, 2)]
-wc1 = SimpleSDMPredictor(WorldClim, BioClim, 1; coords...)
-SimpleSDMLayers._layers_are_compatible(lc1, wc1) # not compatible
+# # lc1 = SimpleSDMPredictor(Copernicus, LandCover, 1; coords...)
+# lc1 = SimpleSDMPredictor(Copernicus, LandCover, 1)[coords]
+# lc1 = lc1[top = coords.top - stride(lc1, 2)]
+# wc1 = SimpleSDMPredictor(WorldClim, BioClim, 1; coords...)
+# SimpleSDMLayers._layers_are_compatible(lc1, wc1) # not compatible
 
 ## Export values to compare
 lc_path = joinpath("assets", "landcover")
@@ -57,4 +45,15 @@ lc_vars = [l[top = coords.top - stride(l, 2)] for l in lc_vars]
 
 # Step 1: SimpleSDMPredictor, as before
 df1 = valuesdf(lc_vars)
-CSV.write(joinpath(lc_path, "landcover_values_df1.csv"), df1)
+# CSV.write(joinpath(lc_path, "landcover_values_df1.csv"), df1)
+df1 = CSV.read(joinpath(lc_path, "landcover_values_df1.csv"), DataFrame)
+
+# Step 2: SimpleSDMPredictor, with integer as default and range as overload
+SimpleSDMPredictor(Copernicus, LandCover)
+SimpleSDMPredictor(Copernicus, LandCover, 1)
+SimpleSDMPredictor(Copernicus, LandCover, 1:10)
+df2 = valuesdf(lc_vars)
+# CSV.write(joinpath(lc_path, "landcover_values_df2.csv"), df2)
+df2 = CSV.read(joinpath(lc_path, "landcover_values_df2.csv"), DataFrame)
+isequal(df1, df2) # true
+# confirmed by comparison
