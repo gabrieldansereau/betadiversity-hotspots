d555b05fe09d9d7aba662ce216f32700cb51106c
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-05-27 18:27:32 -0400

    📝 update landcover loading function docstring

--- src/lib/landcover.jl
+++ src/lib/landcover.jl
@@ -5,9 +5,35 @@ struct Copernicus end
 import SimpleSDMLayers: SimpleSDMPredictor
 
 """
-    SimpleSDMPredictor(Copernicus, LandCover, layer::Int64; x...)
+    SimpleSDMPredictor(::Type{Copernicus}, ::Type{LandCover}, layer::Integer=1; resolution::Float64=10.0, path::AbstractString=joinpath("assets", "landcover"), kwargs...)
+    SimpleSDMPredictor(::Type{Copernicus}, ::Type{LandCover}, layers::AbstractArray; kwargs...)
 
-Return a single landcover layer.
+Prepares the Copernicus v2.0.1 landcover data, and returns them as an array of 
+`SimpleSDMPredictor`s. Layers are called by their number, from 1 to 10. The list
+of available layers is given in the table below.
+
+This function loads the pre-downloaded and coarsened data from the 
+`assets/landcover/` folder (the `path` argument). These steps can be performed 
+with the scripts in `src/shell/` using Bash and GDAL. The available resolutions 
+are 10 and 5 arc-minutes, and must be specified as a floating point value with 
+the `resolution` keyword (with `10.0` as the default).
+
+The original data by Buchhorn et al. is archived on Zenodo at 
+https://zenodo.org/record/3243509, and is available at a much finer resolution.
+
+| Variable | Description        |
+| ------   | ------             |
+| 1        | Bare               |
+| 2        | Crops              |
+| 3        | Grass              |
+| 4        | Moss               |
+| 5        | Shrub              |
+| 6        | Snow               |
+| 7        | Tree               |
+| 8        | Urban              |
+| 9        | Water permanent    |
+| 10       | Water seasonal     |
+
 """
 function SimpleSDMPredictor(::Type{Copernicus}, ::Type{LandCover}, layer::Integer=1; resolution::Float64=10.0, path::AbstractString=joinpath("assets", "landcover"), kwargs...)
     @assert resolution in [5.0, 10.0]
@@ -28,11 +54,6 @@ function SimpleSDMPredictor(::Type{Copernicus}, ::Type{LandCover}, layer::Intege
     return layer
 end
 
-"""
-    SimpleSDMPredictor(Copernicus, LandCover, layers::UnitRange{Int64}; x...)
-
-Return a range of landcover layers.
-"""
 function SimpleSDMPredictor(::Type{Copernicus}, ::Type{LandCover}, layers::AbstractArray; kwargs...)
     @assert eltype(layers) <: Integer
     return [SimpleSDMPredictor(Copernicus, LandCover, l; kwargs...) for l in layers]
