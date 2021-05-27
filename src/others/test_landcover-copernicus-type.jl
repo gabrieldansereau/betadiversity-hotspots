5d23827525d0bb730da6663cd88d006fb2a4339c
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-05-27 13:20:51 -0400

    🚧 export current landcover values

--- src/others/test_landcover-copernicus-type.jl
+++ src/others/test_landcover-copernicus-type.jl
@@ -35,3 +35,22 @@ lc1 = SimpleSDMPredictor(Copernicus, LandCover, 1)[coords]
 lc1 = lc1[top = coords.top - stride(lc1, 2)]
 wc1 = SimpleSDMPredictor(WorldClim, BioClim, 1; coords...)
 SimpleSDMLayers._layers_are_compatible(lc1, wc1) # not compatible
+
+## Export values to compare
+lc_path = joinpath("assets", "landcover")
+function nothingtomissing!(df::DataFrame)
+    allowmissing!(df)
+    for col in eachcol(df)
+        replace!(col, nothing => missing)
+    end
+end
+function valuesdf(layers::Vector{T}) where {T <: SimpleSDMLayer}
+    df = DataFrame(layers)
+    rename!(df, [:longitude, :latitude, Symbol.("lc", 1:10)...])
+    nothingtomissing!(df)
+    return df
+end
+
+# Step 1: SimpleSDMPredictor, as before
+df1 = valuesdf(lc_vars)
+CSV.write(joinpath(lc_path, "landcover_values_1.csv"), df1)
