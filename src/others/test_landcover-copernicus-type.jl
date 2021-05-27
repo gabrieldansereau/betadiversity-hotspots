94c7f6b06ab361d126847cb1a4f2edfb4d2986f8
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-05-27 12:54:11 -0400

    🚧 compatibility test

--- src/others/test_landcover-copernicus-type.jl
+++ src/others/test_landcover-copernicus-type.jl
@@ -21,6 +21,17 @@ lc_vars = landcover(1:10, resolution = 10.0)
 lc_vars = map(x -> landcover(x, resolution = 10.0)[coords], 1:10)
 
 # Test loading variables
-lc_vars = SimpleSDMPredictor(Copernicus, LandCover, 1:10)
 lc_vars = SimpleSDMPredictor(Copernicus, LandCover, 1:10; resolution = 5.0)
+lc_vars = SimpleSDMPredictor(Copernicus, LandCover, 1:10)
 lc_vars = map(x -> SimpleSDMPredictor(Copernicus, LandCover, x)[coords], 1:10)
+
+# Check compatibility
+lc1 = SimpleSDMPredictor(Copernicus, LandCover, 1)
+wc1 = SimpleSDMPredictor(WorldClim, BioClim, 1)
+SimpleSDMLayers._layers_are_compatible(lc1, wc1) # compatible
+
+# lc1 = SimpleSDMPredictor(Copernicus, LandCover, 1; coords...)
+lc1 = SimpleSDMPredictor(Copernicus, LandCover, 1)[coords]
+lc1 = lc1[top = coords.top - stride(lc1, 2)]
+wc1 = SimpleSDMPredictor(WorldClim, BioClim, 1; coords...)
+SimpleSDMLayers._layers_are_compatible(lc1, wc1) # not compatible
