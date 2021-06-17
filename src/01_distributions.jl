714a509c5bc3d9065173bd2c6460b30325c85b13
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-06-17 11:48:22 -0400

    ✨ export raw distributions for QC as tif file

--- src/01_distributions.jl
+++ src/01_distributions.jl
@@ -92,6 +92,10 @@ if (@isdefined save_data) && save_data == true
     touch(joinpath("data", "jld2", "$(outcome)-distributions.jld2"))
     # Export distribution layers
     geotiff(joinpath("data", "proc", "distributions_raw.tif"), distributions)
+    # Export distributions for QC only
+    coords_qc = (left = -80.0, right = -55.0, bottom = 45.0, top = 63.0)
+    distributions_qc = [d[coords_qc] for d in distributions]
+    geotiff(joinpath("data", "proc", "distributions_raw_qc.tif"), distributions_qc)
 
     ## Export to CSV as Y matrix
     @info "Exporting data to CSV as Y matrix ($(outcome) distributions data)"
