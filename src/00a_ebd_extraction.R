5bdf0acefa3f1362e94fc28dc939e009136fc424
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-07-07 16:40:53 -0400

    🔨 update & fix EBD extraction script

--- src/00a_ebd_extraction.R
+++ src/00a_ebd_extraction.R
@@ -1,21 +1,15 @@
 #### Explore eBird Dataset ####
 # Mostly following tutorial from http://strimas.com/ebird-best-practices/
 
-library(conflicted)
+source(file.path("src", "required.R"))
 library(auk)
-library(lubridate)
-library(sf)
-library(gridExtra)
-library(tidyverse)
-library(here)
-
-# resolve namespace conflicts
-conflict_prefer("filter", "dplyr")
-conflict_prefer("select", "dplyr")
 
 # Set ebd path
 # auk::auk_set_ebd_path("~/github/data/ebd/raw/")
 # Restart session
+
+# Conditional evaluations
+# save_prepdata <- TRUE
 
 #### 1. Explore EBD Sample ####
 
@@ -32,12 +26,17 @@ table(ebd_sample$PROTOCOL.TYPE)
 table(ebd_sample$SCIENTIFIC.NAME)
 
 # Check taxonomy
+ebird_taxonomy <- as_tibble(ebird_taxonomy)
 names(ebird_taxonomy)
-head(ebird_taxonomy)
+ebird_taxonomy
 
 # Extract Parulidae taxonomy information
-parulidae_taxonomy <- filter(ebird_taxonomy, family == "Parulidae")
-head(parulidae_taxonomy)
+parulidae_taxonomy <- ebird_taxonomy %>%
+  filter(str_detect(family, "Parulidae"))
+
+# Check parulidae taxonomy
+distinct(parulidae_taxonomy, family)
+parulidae_taxonomy
 View(parulidae_taxonomy)
 
 # Extract Parulidae species names (species only, no hybrid-issf-spuh-...)
@@ -62,7 +61,7 @@ ebd_filters
 # [1:97], [98:110], [14:110] all work
 
 # Export filtered data
-f_ebd <- here("..", "data", "ebd", "processed", "ebd_sample_test.csv")
+f_ebd <- here("data", "proc", "ebd_sample_test.csv")
 auk_filter(ebd_filters, file = f_ebd, overwrite = T)
 
 # Test exported file
@@ -75,26 +74,27 @@ table(test$PROTOCOL.TYPE)
 #### 3. Extract data from complete database ####
 
 # Check worldwide distribution for a few species (manually from eBird website)
-amN <- c("Myioborus miniatus", "Myioborus pictus",
-     "Cardellina rubrifrons")
-amS <- c("Myioborus albifrons", "Myioborus melanocephalus",
-     "Myioborus ornatus", "Myioborus flavivertex",
-     "Myioborus pariae", "Myioborus castaneocapilla",
-     "Myioborus brunniceps", "Myiothlypis rivularis",
-     "Myiothlypis nigrocristata", "Myiothlypis leucoblephara",
-     "Myiothlypis luteoviridis")
-amC <- c("Myioborus torquatus", "Cardellina versicolor",
-     "Cardellina rubra")
+amN <- c("Myioborus miniatus", "Myioborus pictus", "Cardellina rubrifrons")
+amS <- c(
+  "Myioborus albifrons", "Myioborus melanocephalus",
+  "Myioborus ornatus", "Myioborus flavivertex",
+  "Myioborus pariae", "Myioborus castaneocapilla",
+  "Myioborus brunniceps", "Myiothlypis rivularis",
+  "Myiothlypis nigrocristata", "Myiothlypis leucoblephara",
+  "Myiothlypis luteoviridis"
+  )
+amC <- c("Myioborus torquatus", "Cardellina versicolor", "Cardellina rubra")
 nul <- c("Myioborus albifacies", "Myioborus cardonai")
-# Remove a few species not present in North America (to have 97 at most and run command onloy once)
+
+# Remove a few species not present in North America (to have 97 at most and run command only once)
 to_remove <- c(amS, nul)
 fewer_parulidae_species <- parulidae_species[!(parulidae_species %in% to_remove)]
 fewer_parulidae_species
 
 # Get ebd file
-ebd <- auk_ebd("ebd_relJun-2019.txt",
-         file_sampling = "ebd_sampling_relJun-2019.txt")
+ebd <- auk_ebd("ebd_relJun-2019.txt", file_sampling = "ebd_sampling_relJun-2019.txt")
 ebd_sampling <- auk_sampling("ebd_sampling_relJun-2019.txt")
+
 # Apply filters
 ebd_filters <- ebd %>%
   auk_species(fewer_parulidae_species) %>%
@@ -105,11 +105,17 @@ ebd_sampling_filters <- ebd_sampling %>%
   auk_country(c("CA", "US", "MX")) %>%
   auk_complete()
 ebd_sampling_filters
+
 # Export filtered data !!! SEVERAL HOURS !!!!
-f_ebd <- here("..", "data", "ebd", "processed", "ebd_warblers.csv")
-f_sampling <- here("..", "data", "ebd", "processed", "ebd_warblers_sampling.csv")
-auk_filter(ebd_filters, file = f_ebd, file_sampling = f_sampling)
-auk_filter(ebd_sampling_filters, file = f_sampling)
+# (Only if save_prepdata option correctly set)
+# save_prepdata <- TRUE
+if (exists("save_prepdata") && isTRUE(save_prepdata)) {
+  message("Exporting filtered EBD data sets")
+  f_ebd <- here("..", "data", "ebd", "processed", "ebd_warblers.csv")
+  f_sampling <- here("..", "data", "ebd", "processed", "ebd_warblers_sampling.csv")
+  auk_filter(ebd_filters, file = f_ebd, file_sampling = f_sampling)
+  auk_filter(ebd_sampling_filters, file = f_sampling)
+}
 # File is too big, needs to cut in terminal
 
 #### 4. Cut file using bash command ####
@@ -118,28 +124,35 @@ auk_filter(ebd_sampling_filters, file = f_sampling)
 vars <- c("GLOBAL.UNIQUE.IDENTIFIER", "CATEGORY", "SCIENTIFIC.NAME", "SUBSPECIES.SCIENTIFIC.NAME", "OBSERVATION.COUNT", "COUNTRY.CODE",
       "LATITUDE", "LONGITUDE", "OBSERVATION.DATE", "OBSERVER.ID", "SAMPLING.EVENT.IDENTIFIER", "PROTOCOL.TYPE", "DURATION.MINUTES",
       "EFFORT.DISTANCE.KM", "NUMBER.OBSERVERS", "ALL.SPECIES.REPORTED", "GROUP.IDENTIFIER", "APPROVED")
+
 # Get column indices
 inds <- which(names(ebd_sample) %in% vars)
 inds
 
-## Test command with smaller dataset
 # Desired command
 # cut -f1,4,5,6,8,9,14,26,27,28,30,31,32,35,36,38,39,40,42 data/raw/ebd_warblers.csv > data/raw/ebd_warblers_cut.csv
-# Create command
-bash_command_test <- paste("cut -f", paste(inds, collapse=","), " ../data/ebd/processed/ebd_sample_test.csv > ../data/ebd/processed/rbashtest.csv", sep="")
+
+# Test command with smaller dataset
+bash_command_test <- paste("cut -f", paste(inds, collapse=","), " ./data/proc/ebd_sample_test.csv > ./data/proc/ebd_sample_cut.csv", sep="")
 bash_command_test
-# Run command
+
+# Run test command
 system(bash_command_test)
+
 # Check result
-bash_test <- read_tsv(here("..", "data", "ebd", "processed", "rbashtest.csv"))
+bash_test <- read_tsv(here("data", "proc", "ebd_sample_cut.csv"))
 head(bash_test)
 
-## Run for full dataset
-# Create command
+# Create command for full data set
 bash_command <- paste("cut -f", paste(inds, collapse=","), " ../data/ebd/processed/ebd_warblers.csv > ../data/ebd/processed/ebd_warblers_cut.csv", sep="")
 bash_command
-# Run command and calculate runtime
-ptm <- proc.time()
-system(bash_command)
-proc.time() - ptm
 
+# Run command on full data set and calculate runtime
+# (Only if save_prepdata option correctly set)
+# save_prepdata <- TRUE
+if (exists("save_prepdata") && isTRUE(save_prepdata)) {
+  message("Cutting full EBD data set")
+  tic("Cutting full EBD data data set")
+  system(bash_command)
+  toc()
+}
