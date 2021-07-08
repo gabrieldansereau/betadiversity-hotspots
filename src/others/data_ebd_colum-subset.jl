0e86de9e26140b57e5774ea64d966ab27fefb1c1
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-07-07 20:09:04 -0400

    🔥 remove useless codes in column subset script

--- src/others/data_ebd_colum-subset.jl
+++ src/others/data_ebd_colum-subset.jl
@@ -6,22 +6,12 @@ include("../required.jl")
 # Set path to files
 cut_file = "./data/raw/ebd_warblers_cut.csv"
 head_file = "./data/proc/ebd_warblers_cut_head.csv"
-new_file = "./data/proc/ebd_warblers_recut.csv"
 
-# Create head csv files to get column names
-# cut_cmd = `head $cut_file \> $head_file`
-# run(cut_cmd) # pipelines don't work directly in Julia
-run(pipeline(`head $cut_file`, head_file))
-
-# Test read
-@time head1 = CSV.read(head_file, DataFrame)
-
-# Even simpler: load first rows only
-@time head2 = CSV.read(cut_file, DataFrame; limit=9)
-isequal(head1, head2)
+# Load first rows only
+@time head1 = CSV.read(cut_file, DataFrame; limit=9)
 
 # Select subset of names
-show(stdout, "text/plain", names(head2))
+show(stdout, "text/plain", names(head1))
 names_subset = [
     # "GLOBAL UNIQUE IDENTIFIER",
     # "CATEGORY",
@@ -43,14 +33,14 @@ names_subset = [
     "GROUP IDENTIFIER",
     "APPROVED",
 ]
-findall(in(names_subset), names(head2))
+findall(in(names_subset), names(head1))
 
 # Load names subset on head lines first (to test)
-head3 = CSV.read(cut_file, DataFrame; limit=9, select=names_subset)
+head2 = CSV.read(cut_file, DataFrame; limit=9, select=names_subset)
 
 # Test prepare_ebd_data! function on selected columns
-prepare_ebd_data!(head3)
-select!(head3, [:species, :commonName, :year, :latitude, :longitude, :groupIdentifier])
+prepare_ebd_data!(head2)
+select!(head2, [:species, :commonName, :year, :latitude, :longitude, :groupIdentifier])
 
 # Load names subset on full data set (to see if it fit in memory)
 @time df = CSV.read(cut_file, DataFrame; select=names_subset)
